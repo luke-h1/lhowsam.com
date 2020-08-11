ea5f3921c4d45f442feb34e73f3b5c1ca7d4e349
luke-h1 <luke.howsam@yahoo.com>
2020-08-11 20:33:34 +0100

    update

--- app.js
+++ app.js
@@ -2,45 +2,25 @@ const themeDots = document.getElementsByClassName('theme-dot');
 const theme = localStorage.getItem('theme');
 
 if (theme == null) {
-  setTheme('light'); // set default theme
+    setTheme('light'); // set default theme
 } else {
-  setTheme(theme); // theme of user's choice
+    setTheme(theme); // theme of user's choice
 }
 
 for (let i = 0; themeDots.length > i; i++) {
-  themeDots[i].addEventListener('click', function () {
-    const { mode } = this.dataset;
-    console.log('option-clicked:', mode);
-    setTheme(mode);
-  });
+    themeDots[i].addEventListener('click', function () {
+        const { mode } = this.dataset;
+        console.log('option-clicked:', mode);
+        setTheme(mode);
+    });
 }
 
 function setTheme(mode) {
-  if (mode == 'light') {
-    document.getElementById('theme-style').href = 'styles.css';
-  } else if (mode == 'blue') {
-    document.getElementById('theme-style').href = 'blue.css';
-  } else if (mode == 'green') {
-    document.getElementById('theme-style').href = 'green.css';
-  } else if (mode == 'purple') {
-    document.getElementById('theme-style').href = 'purple.css';
-  } else {
-    document.getElementById('theme-style').href = 'dark-mode.css';
-  }
-
-  localStorage.setItem('theme', mode); // set values to be stored in local storage
+    if (mode == 'light') {
+        document.getElementById('theme-style').href = 'styles.css';
+    } else {
+        document.getElementById('theme-style').href = 'dark-mode.css';
+    }
+    localStorage.setItem('theme', mode); // set values to be stored in local storage
 }
 
-// // typing effect
-// let i = 0;
-// const txt = 'theme options will be saved for your next visit !';
-// const speed = 50; // speed in miliseconds
-
-// function animation() {
-//   if (i < txt.length) {
-//     document.getElementById('settings-note').innerHTML += txt.charAt(i);
-//     i++;
-//     setTimeout(animation, speed);
-//   }
-// }
-// animation();
