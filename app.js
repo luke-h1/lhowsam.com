681cd7faff45fc1141998f5388167bfdcc449ce1
luke-h1 <luke.howsam@yahoo.com>
2020-08-15 22:29:02 +0100

    add new theme switcher. It's broken

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-  
-const themeDots = document.getElementsByClassName('theme-dot');
-const theme = localStorage.getItem('theme');
+const check = document.querySelector('.checkbox'); 
+check.addEventListener('change', () => { 
+    console.log('this function does fuck all !')
+})
 
-if (theme == null) {
-    setTheme('light'); // set default theme
-} else {
-    setTheme(theme); // theme of user's choice
-}
 
-for (let i = 0; themeDots.length > i; i++) {
-    themeDots[i].addEventListener('click', function () {
-        const { mode } = this.dataset;
-        console.log('option-clicked:', mode);
-        setTheme(mode);
-    });
-}
 
-function setTheme(mode) {
-    if (mode == 'light') {
-        document.getElementById('theme-style').href = 'styles.css';
-    } else {
-        document.getElementById('theme-style').href = 'dark-mode.css';
-    }
-    localStorage.setItem('theme', mode); // set values to be stored in local storage
-}
+// old theme switcher (unused)
+
+// const themeDots = document.getElementsByClassName('theme-dot');
+// const theme = localStorage.getItem('theme');
+
+// if (theme == null) {
+//     setTheme('light'); 
+// } else {
+//     setTheme(theme); 
+// }
+
+// for (let i = 0; themeDots.length > i; i++) {
+//     themeDots[i].addEventListener('click', function () {
+//         const { mode } = this.dataset;
+//         console.log('option-clicked:', mode);
+//         setTheme(mode);
+//     });
+// }
+
+// function setTheme(mode) {
+//     if (mode == 'light') {
+//         document.getElementById('theme-style').href = 'styles.css';
+//     } else {
+//         document.getElementById('theme-style').href = 'dark-mode.css';
+//     }
+//     localStorage.setItem('theme', mode); // set values to be stored in local storage
+// }
