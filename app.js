1417d38ddfb8a44b4a9342bce1a05578895e4bf8
luke-h1 <luke.howsam@yahoo.com>
2020-08-19 18:45:59 +0100

    feature 🚀: Implement localstorage saving for theme switcher

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const checkbox = document.getElementById('checkbox1');
+const theme = localStorage.getItem('theme');
 checkbox.addEventListener('change', darkModeSwitcher);
 
-function darkModeSwitcher() { // TODO: SAVE IN LOCAL STORAGE THE THEME THAT IS PICKED 
+if (theme === null) {
+    setTheme('light');
+} else {
+    setTheme(theme);
+}
+
+function darkModeSwitcher() {
+    // TODO: SAVE IN LOCAL STORAGE THE THEME THAT IS PICKED
     if (checkbox.checked) {
         console.log('checkbox has been checked');
-        document.getElementById('theme-style').href = 'dark-mode.css'; 
+        document.getElementById('theme-style').href = 'dark-mode.css';
+        setTheme('dark'); 
     } else {
+        setTheme('light')
         console.log('not checked...');
         document.getElementById('theme-style').href = 'styles.css';
     }
 }
 
+function setTheme(mode) {
+    if (mode === 'light') {
+        document.getElementById('theme-style').href = 'styles.css';
 
+    } else {
+        document.getElementById('theme-style').href = 'dark-mode.css';
+    }
+    localStorage.setItem('theme', mode)
 
+}
 
 // old theme switcher (unused)
 
