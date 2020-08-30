daffd4725aba0166869be49e6e0675f7c5ee845f
luke-h1 <luke.howsam@yahoo.com>
2020-08-30 13:42:34 +0100

    remove un used js

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-/*
-==============
-THEME SWITCHER 
-==============
-*/
-
-
 const checkbox = document.getElementById('checkbox1');
 const theme = localStorage.getItem('theme');
-checkbox.addEventListener('change', darkModeSwitcher);
 
 
 if (theme === null) {
@@ -35,12 +27,7 @@ function setTheme(mode) {
   localStorage.setItem('theme', mode);
 }
 
-/*
-==============
-SCROLL TO TOP 
-==============
-*/
-
+// Scroll To Top BTN 
 const rootEl = document.documentElement;
 const topBtn = document.querySelector('.scrollToTopBtn');
 
@@ -58,33 +45,10 @@ function handleScroll() {
     topBtn.style.display = 'none';
   }
 }
+
+
+
+// EVENT LISTENERS 
+checkbox.addEventListener('change', darkModeSwitcher);
 document.addEventListener('scroll', handleScroll);
 
-
-
-// old theme switcher (unused)
-// const themeDots = document.getElementsByClassName('theme-dot');
-// const theme = localStorage.getItem('theme');
-
-// if (theme == null) {
-//     setTheme('light');
-// } else {
-//     setTheme(theme);
-// }
-
-// for (let i = 0; themeDots.length > i; i++) {
-//     themeDots[i].addEventListener('click', function () {
-//         const { mode } = this.dataset;
-//         console.log('option-clicked:', mode);
-//         setTheme(mode);
-//     });
-// }
-
-// function setTheme(mode) {
-//     if (mode == 'light') {
-//         document.getElementById('theme-style').href = 'styles.css';
-//     } else {
-//         document.getElementById('theme-style').href = 'dark-mode.css';
-//     }
-//     localStorage.setItem('theme', mode); // set values to be stored in local storage
-// }
