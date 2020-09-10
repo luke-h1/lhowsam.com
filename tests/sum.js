be39b2de762cee2a31fe584b4e2b4e488f0abcf3
luke-h1 <luke.howsam@yahoo.com>
2020-09-10 16:30:24 +0100

    adjust some styles

--- tests/sum.js
+++ tests/sum.js
@@ -2,3 +2,7 @@ function sum(a, b){
   return a + b;
 }
 module.exports = sum;
+
+
+// todo: write tests to test the scroll to top 
+// todo: write tests to test the dark-mode switcher (todo after the above) 
