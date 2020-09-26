a60e2d9f4c07743dc5d49f1c01fb351053028b84
luke-h1 <luke.howsam@yahoo.com>
2020-09-26 16:57:13 +0100

    add scroll to top button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const Footer = () => {
+  const scrollClick = () => {
+    const rootEl = document.documentElement;
+
+    rootEl.scrollTo({ top: 0, behavior: 'smooth' });
+  };
   return (
     <div>
-      {/* <button className='scrollToTopBtn'>
-        <i className='fa fa-angle-up up-icon' aria-hidden='true'></i>
-      </button> */}
+      <div className='btn-container'>
+        <button className='scrollToTopBtn' onClick={scrollClick}>
+          <i className='fa fa-angle-up up-icon' aria-hidden='true'></i>
+        </button>
+      </div>
 
       <section className='s2'>
         <div className='main-container'>
