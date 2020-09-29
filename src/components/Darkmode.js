5b596cfbbaf1f2694ea0064bb85b3196d9f721e1
luke-h1 <luke.howsam@yahoo.com>
2020-09-29 16:58:31 +0100

    fix linting

--- src/components/Darkmode.js
+++ src/components/Darkmode.js
@@ -1,42 +1,42 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect } from "react";
 
 const Darkmode = () => {
   useEffect(() => {
-    const currentTheme = localStorage.getItem('theme');
+    const currentTheme = localStorage.getItem("theme");
 
     if (currentTheme) {
-      document.documentElement.setAttribute('data-theme', currentTheme);
+      document.documentElement.setAttribute("data-theme", currentTheme);
 
-      if (currentTheme === 'dark') {
-        document.documentElement.setAttribute('data-theme', 'dark');
+      if (currentTheme === "dark") {
+        document.documentElement.setAttribute("data-theme", "dark");
       }
     }
   }, []);
 
   const onClick = (e) => {
     if (e.target.checked) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-      localStorage.setItem('theme', 'dark');
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
     } else if (!e.target.checked) {
-      document.documentElement.setAttribute('data-theme', 'light');
-      localStorage.setItem('theme', 'light');
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light");
     }
   };
 
   return (
     <Fragment>
-      <div className='theme-switcher'>
+      <div className="theme-switcher">
         <input
-          type='checkbox'
-          className='checkbox'
-          id='checkbox'
-          name='checkbox'
+          type="checkbox"
+          className="checkbox"
+          id="checkbox"
+          name="checkbox"
           onClick={onClick}
         />
-        <label className='label' htmlFor='checkbox'>
-          <i className='fa fa-moon'></i>
-          <i className='fa fa-sun'></i>
-          <div className='ball'></div>
+        <label className="label" htmlFor="checkbox">
+          <i className="fa fa-moon"></i>
+          <i className="fa fa-sun"></i>
+          <div className="ball"></div>
         </label>
       </div>
     </Fragment>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   const scrollClick = () => {
     const rootEl = document.documentElement;
 
-    rootEl.scrollTo({ top: 0, behavior: 'smooth' });
+    rootEl.scrollTo({ top: 0, behavior: "smooth" });
   };
   return (
     <div>
-      <div className='btn-container'>
-        <button className='scrollToTopBtn' onClick={scrollClick}>
-          <i className='fa fa-angle-up up-icon' aria-hidden='true'></i>
+      <div className="btn-container">
+        <button className="scrollToTopBtn" onClick={scrollClick}>
+          <i className="fa fa-angle-up up-icon" aria-hidden="true"></i>
         </button>
       </div>
 
-      <section className='s2'>
-        <div className='main-container'>
-          <h3 className='h3-center'>Contact Me</h3>
-          <div className='icon-container'>
+      <section className="s2">
+        <div className="main-container">
+          <h3 className="h3-center">Contact Me</h3>
+          <div className="icon-container">
             <a
-              className='social-list__link'
-              href='https://github.com/luke-h1/'
-              target='_blank'
-              rel='noopener noreferrer'
+              className="social-list__link"
+              href="https://github.com/luke-h1/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <i className='fa fa-github fa-3x' aria-hidden='true'></i>
+              <i className="fa fa-github fa-3x" aria-hidden="true"></i>
             </a>
             <a
-              className='social-list__link'
-              href='https://www.linkedin.com/in/lukehowsam/'
-              target='_blank'
-              rel='noopener noreferrer'
+              className="social-list__link"
+              href="https://www.linkedin.com/in/lukehowsam/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <i className='fa fa-linkedin fa-3x' aria-hidden='true'></i>
+              <i className="fa fa-linkedin fa-3x" aria-hidden="true"></i>
             </a>
           </div>
         </div>
