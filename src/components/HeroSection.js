50cf4d49adad693827d8ea4144191d370ad78d9b
luke-h1 <luke.howsam@yahoo.com>
2020-10-10 16:31:24 +0100

    refactor herosection to use prop-types

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
 import profileImage from '../images/luke.png';
 import './HeroSection.css';
-import {FaGithub, FaLinkedin} from 'react-icons/fa';
-function HeroSection({ lightBg }) {
+import PropTypes from 'prop-types';
+function HeroSection({ lightBg, title, skills, introText }) {
   return (
     <Fragment>
       <div className={'home__hero-section darkBg'}>
@@ -10,12 +10,8 @@ function HeroSection({ lightBg }) {
           <div className="row home__hero-row">
             <div className="col">
               <div className="home__hero-text-wrapper">
-                <div className="top-line">Hi, I'm Luke</div>
-                <p className="paragraph">
-                  I'm passionate about solving technical problems and coming up
-                  with creative solutions.I enjoy working with the JAMstack &
-                  various automation technologies.
-                </p>
+                <div className="top-line">{title}</div>
+                <p className="paragraph">{introText}</p>
               </div>
             </div>
             <div className="col">
@@ -24,14 +20,11 @@ function HeroSection({ lightBg }) {
                   src={profileImage}
                   alt="profile card"
                   className="home__hero-img"
-                /> 
-              {/* <FaGithub className='icon-social'/> 
-              <FaLinkedin className='icon-social' />  */}
-              </div>  
-            
+                />
+              </div>
             </div>
             <div className="skills-container">
-              <h1 className="skills">Top Skills</h1>
+              <h1 className="skills">{skills}</h1>
               <p>Automation (ansible / bash scripting)</p>
               <p>HTML, CSS</p>
               <p>Basic vanilla Javascript</p>
@@ -44,5 +37,19 @@ function HeroSection({ lightBg }) {
     </Fragment>
   );
 }
+
+HeroSection.defaultProps = {
+  title: `Hi, I'm Luke`,
+  skills: 'Top Skills',
+  introText: `I'm passionate about solving technical problems and coming up
+  with creative solutions.I enjoy working with the JAMstack &
+  various automation technologies.`,
+};
+
+HeroSection.propTypes = {
+  title: PropTypes.string.isRequired,
+  skills: PropTypes.string.isRequired,
+  introText: PropTypes.string.isRequired,
+};
 
 export default HeroSection;
