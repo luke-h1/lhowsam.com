fef25198846dfc3ffef9161bfdb6f3e8eee57d71
luke-h1 <luke.howsam@yahoo.com>
2020-10-25 20:55:53 +0000

    get rid of skills and start again

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react';
 import Cards from '../../components/Cards/Cards';
 import HeroSection from '../../components/HeroSection/HeroSection';
-import Skills from '../../components/Skills/Skills';
+// import Skills from '../../components/Skills/Skills';
 import Contact from '../../components/Contact/Contact';
 const Home = () => {
   return (
     <Fragment>
       <HeroSection />
-      <Skills />
+      {/* <Skills /> */}
       <Cards />
       <Contact />
     </Fragment>
