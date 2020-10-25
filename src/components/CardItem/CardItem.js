c03c15d261fabe6cd666fd43f93afe143522b9fa
luke-h1 <luke.howsam@yahoo.com>
2020-10-25 12:03:53 +0000

    finish cardItem

---
+++ src/components/CardItem/CardItem.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+const CardItem = (props) => {
+  return (
+    <Fragment>
+      <li className="cards__item">
+        <Link className="cards__item__link" to={props.path}>
+          <figure className="cards__item__pic-wrap" data-category={props.label}>
+            <img src={props.src} alt="Travel" className="cards__item__img" />
+          </figure>
+          <div className="cards__item__info">
+            <h5 className="cards__item__text">{props.text}</h5>
+          </div>
+        </Link>
+      </li>
+    </Fragment>
+  );
+};
+export default CardItem
