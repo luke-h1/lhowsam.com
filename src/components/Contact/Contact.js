f5a2258692b15c0a8ea16d87f143ec3b6aeab383
luke-h1 <luke.howsam@yahoo.com>
2020-10-27 13:30:09 +0000

    update to contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,7 @@ import { IconContext } from 'react-icons/lib';
 import './Contact.scss';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import PropTypes from 'prop-types';
-
+import {AiOutlineMail} from 'react-icons/ai';
 // TODO: ADD CONTACT FORM WITH VALIDATION HERE....
 
 const Contact = ({ title, MainTitle }) => {
@@ -16,6 +16,9 @@ const Contact = ({ title, MainTitle }) => {
             <h3>{title}</h3>
             <br />
             <div className="icon-wrapper">
+              <a href='mailto:luke.howsam@yahoo.com'>
+                <AiOutlineMail className='icon-contact email'/> 
+              </a>
               <a
                 href="https://www.linkedin.com/in/lukehowsam/"
                 target="_blank"
