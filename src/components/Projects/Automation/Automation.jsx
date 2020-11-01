7959dd888f73fa14fff2aec9f9387f4905ef40e9
luke-h1 <luke.howsam@yahoo.com>
2020-11-01 15:04:35 +0000

    feat/general-cleanup

--- src/components/Projects/Automation/Automation.jsx
+++ src/components/Projects/Automation/Automation.jsx
@@ -11,12 +11,12 @@ const Automation = () => {
         title="Automation"
         src={Image3}
         alt="City Traffic"
-        desc="I am a huge advocate for infrastructure automation as this
-        allows teams to focus on innovating new features and solving new
-        problems rather than being backlogged with maintaining
+        desc="I am a huge advocate for infrastructure automation & continuous integration as this
+        allows developerrs to focus on innovating new features and solving new
+        problems rather than being backlogged with maintaining existing
         infrastructure. In this project I made Ansible playbooks & Bash
         scripts that automate the setup of Linux & Mac OS X workstations
-        & servers"
+        & servers. In addition to this I also setup an azure pipeline to run on pull requests made to the repository to ensure that any features added to the playbooks have no errors."
         label="Ansible, Bash, CI"
         github="https://github.com/luke-h1/Automation"
         site=""
