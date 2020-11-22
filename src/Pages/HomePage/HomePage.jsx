7d79280b6142e66c0c58be00b5eb85d390f9e69e
luke-h1 <luke.howsam@yahoo.com>
2020-11-22 14:13:56 +0000

    themeprovider setup

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import Footer from '../../components/Footer/Footer';
 import Hero from '../../components/Hero/Hero';
 import Navbar from '../../components/Navbar/Navbar';
 import ProjectShowCase from '../../components/ProjectShowCase/ProjectShowCase';
+import { GlobalStyles, darkTheme, lightTheme, light } from '../../styles'
 const Home = () => {
+
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <ProjectShowCase /> 
-      <Footer />
-    </>
+    <ThemeProvider theme={usedarkTheme ? darkTheme : lightTheme}>
+      <>
+        <Navbar />
+        <Hero />
+        <ProjectShowCase />
+        <Footer />
+      </>
+    </ThemeProvider>
   );
 };
 export default Home;
---
+++ src/styles/index.jsx
@@ -0,0 +1,4 @@
+export * from './Colors';
+export * from './typography';
+export * from './Themes';
+export * from './GlobalStyles';
