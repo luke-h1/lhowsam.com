95a11601d6d97ef8c1eb7e5e46a68f9069d8e64e
luke-h1 <luke.howsam@yahoo.com>
2020-11-22 13:58:01 +0000

    start to implement global styles with styled-components

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,21 @@
+import {createGlobalStyle} from 'styled-components';
+import {primaryFont} from './typography';
+import {normalize} from 'polished';
+
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize()}
+  * {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font-family: ${primaryFont};
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+
+`
