624ab94a5f7db2c4e3ea13699237dcc8454483c5
luke-h1 <luke.howsam@yahoo.com>
2020-11-22 17:29:33 +0000

    looping over sample blog data

--- src/components/BlogItem/BlogItem.jsx
+++ src/components/BlogItem/BlogItem.jsx
@@ -4,24 +4,26 @@ import {
   BlogCard,
   BlogH1,
   ShortDesc,
-  BlogP
+  BlogP,
 } from './BlogItemElements';
 import Button from '../utils/Buttons/ButtonElements';
 import Img from '../../images/Projects/Coffee-jon-tyson.jpeg';
-
+import { BlogData } from '../../data/BlogData';
 const BlogItem = () => {
   return (
     <>
-      <BlogContainer>
-        <BlogCard>
-          <BlogH1>Title</BlogH1>
-          <ShortDesc>
-            <BlogP>Lorem ipsum dolor sit amet consectetur adipisicing elit. In sunt, delectus ipsum quidem soluta laboriosam veniam recusandae asperiores deserunt totam accusantium eos saepe optio vitae perferendis at eligendi repellendus mollitia.</BlogP>
-
-          </ShortDesc>
-        </BlogCard>
-      </BlogContainer>
-
+      {BlogData.map((blog, i) => {
+        return (
+          <BlogContainer>
+            <BlogCard>
+              <BlogH1>{blog.name}</BlogH1>
+              <ShortDesc>
+                <BlogP>{blog.desc}</BlogP>
+              </ShortDesc>
+            </BlogCard>
+          </BlogContainer>
+        );
+      })}
     </>
   );
 };
---
+++ src/data/BlogData.js
@@ -0,0 +1,20 @@
+import ImageTest from '../images/Projects/Coffee-jon-tyson.jpeg';
+
+export const BlogData = [
+  {
+    img: ImageTest,
+    alt: 'test',
+    name: 'blog post one',
+    desc:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium assumenda voluptatem nobis aliquam ex non, ipsam eveniet, quos dolores totam possimus aperiam itaque quod eos rem velit nesciunt, est exercitationem?',
+    button: 'Go to post',
+  },
+  {
+    img: ImageTest,
+    alt: 'test',
+    name: 'blog post one',
+    desc:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium assumenda voluptatem nobis aliquam ex non, ipsam eveniet, quos dolores totam possimus aperiam itaque quod eos rem velit nesciunt, est exercitationem?',
+    button: 'Go to post',
+  },
+];
