2e04d519d75ddecc882d3f0638e1e5389ef72257
luke-h1 <luke.howsam@yahoo.com>
2020-11-24 20:35:30 +0000

    feat/add-dark-mode-state

--- src/components/utils/Icons/CodeIcon.jsx
+++ src/components/utils/Icons/CodeIcon.jsx
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 import { BiCodeBlock } from 'react-icons/bi';
 import { lightTheme, darkTheme } from '../../../styles/Themes';
 export const CodeIcon = styled(BiCodeBlock)`
-  color: ${lightTheme ? lightTheme.textColor : lightTheme.textColor};
+  color: ${lightTheme ? lightTheme.textColor : darkTheme.textColor};
   font-size: 24px;
 `;
