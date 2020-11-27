5aeb32d46e1eff6cedf71c22ea83999c55ea967e
luke-h1 <luke.howsam@yahoo.com>
2020-11-27 10:16:05 +0000

    resolve dependency issues affecting deploying to prod

--- src/components/BlogItem/__tests__/BlogItem.test.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import BlogItem from '../BlogItem';
-
-test('renders the BlogItem component', () => { 
-  const blog = render(<BlogItem />)
-  expect(blog).toBeInTheDocument();
-})
