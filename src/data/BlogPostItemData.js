c7b9598dc2789379c925bf1a45c6c6cccbfd852b
luke-h1 <luke.howsam@yahoo.com>
2020-11-27 22:21:34 +0000

    update to blog data

--- src/data/BlogPostItemData.js
+++ src/data/BlogPostItemData.js
@@ -3,7 +3,7 @@ import BlogImage1 from '../assets/images/BlogImages/BlogImage1.jpeg';
 export const BlogData = [
   {
     title: 'Hello World',
-    postedAt: '22nd November 2020',
+    postedAt: '20th November 2020',
     slug: '/blog/hello-world',
     image: BlogImage1,
     button: 'Read More',
