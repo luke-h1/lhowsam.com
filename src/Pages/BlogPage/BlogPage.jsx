9f49cc65f698364e6baaddeae0bba025af838cbc
luke-h1 <luke.howsam@yahoo.com>
2020-11-28 13:11:09 +0000

    implement theme-provider across all pages

--- src/Pages/BlogPage/BlogPage.jsx
+++ src/Pages/BlogPage/BlogPage.jsx
@@ -1,18 +1,34 @@
+/* eslint-disable */
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import BlogItem from '../../components/BlogItem/BlogItem';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
+import { useDarkTheme } from '../../hooks/useDarkMode';
+import { lightTheme, darkTheme } from '../../styles/Themes';
+import { ThemeProvider } from 'styled-components';
 
-const BlogPage = () => (
+const BlogPage = () => {
+  const [theme, setTheme] = useDarkTheme(
+    (typeof window !== 'undefined' && window.localStorage.getItem('theme')) || 'light',
+  );
+
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  };
+
+return (
+  <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
   <>
     <Helmet>
       <title>Blog</title>
       <meta name="description" content="Blog page" />
     </Helmet>
-    <Navbar />
-    <BlogItem />
-    <Footer />
+    <Navbar theme={theme} toggleTheme={themeToggler}/>
+    <BlogItem theme={theme} />
+    <Footer theme={theme} />
   </>
+  </ThemeProvider>
 );
+}
 export default BlogPage;
--- src/Pages/Contact/ContactPage.jsx
+++ src/Pages/Contact/ContactPage.jsx
@@ -1,18 +1,35 @@
+/* eslint-disable */
 import React from 'react';
 import Helmet from 'react-helmet';
 import Contact from '../../components/Contact/Contact';
 import Footer from '../../components/Footer/Footer';
 import Navbar from '../../components/Navbar/Navbar';
+import { useDarkTheme } from '../../hooks/useDarkMode';
+import { lightTheme, darkTheme } from '../../styles/Themes';
+import { ThemeProvider } from 'styled-components';
 
-const ContactPage = () => (
+const ContactPage = () =>{
+  const [theme, setTheme] = useDarkTheme(
+    (typeof window !== 'undefined' && window.localStorage.getItem('theme')) || 'light',
+  );
+
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  };
+
+
+return (
+  <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
   <>
     <Helmet>
       <title>Contact</title>
       <meta name="description" content="Contact page" />
     </Helmet>
-    <Navbar />
-    <Contact />
-    <Footer />
+    <Navbar theme={theme} toggleTheme={themeToggler}/>
+    <Contact theme={theme} />
+    <Footer theme={theme} />
   </>
+  </ThemeProvider>
 );
+}
 export default ContactPage;
