36e3970e5ac6df489143e170d4032fdd134c9124
luke-h1 <luke.howsam@yahoo.com>
2020-11-29 17:40:32 +0000

    fix jest config

--- src/__tests__/Hero.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Hero from '../components/Hero/Hero';
-
-test('it renders properly', () => {
-  const container = ReactDOM.render(<Hero />);
-  expect(container).toMatchSnapshot();
-});
