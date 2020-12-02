f24b92119c74eb54537b970c312f298a0dc51936
luke-h1 <luke.howsam@yahoo.com>
2020-12-02 21:48:52 +0000

    fix mistake

--- src/components/ProjectPreview/ProjectPreview.jsx
+++ src/components/ProjectPreview/ProjectPreview.jsx
@@ -6,6 +6,8 @@ import React from 'react'
 const ProjectPreview = ({ Project }) => (
   <>
         {/* TODO: LOOP THRU & SHOW CARD ITEM FOR EACH PROJECT  */}
+        
+        {/* TODO: CREATE JSON OBJ TO LOOP THRU AND CREATE A PAGE BASED ON RESULTS */}
   </>
 )
 
