970af257279bc59cb932bae1dc1781f6e4c96aba
luke-h1 <luke.howsam@yahoo.com>
2020-12-02 21:47:14 +0000

    abstract styles from post preview to post preview elements

--- src/components/PostPreview/PostPreview.jsx
+++ src/components/PostPreview/PostPreview.jsx
@@ -1,58 +1,31 @@
 /* eslint-disable */
 import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'gatsby'
-import ReadLink from '../../components/utils/ReadLink/ReadLink';
-import Image from 'gatsby-image'
-const StyledArticle = styled.article`
-  margin-top: 0;
-  padding-bottom: 1rem;
-  :first-of-type {
-   margin-top: 1rem;
-      }  
-
-   
-`
-
-const Wrap = styled.div`
-`
-
-const styledLink = styled(Link)`
-  margin: 1rem 1rem 0 0;
-        width: 100px;
-`
-
-const StyledImage = styled(Image)`
-    max-height: 200px;
-    border-radius: 10px;
-    margin-top: 0;
-
-  * {
-    margin-top: 0;
-  }
-`
-
-  
-const StyledP = styled.p`
-`
+import {
+  PostArticle,
+  PostWrap,
+  PostImage,
+  PostParagraph,
+  PostLink
+} from './PostPreviewElements';
+import ReadLink from '../utils/ReadLink/ReadLink';
 
 const PostPreview = ({ post }) => (
   <>
-    <StyledArticle>
-      <Wrap>
-      <styledLink to={post.slug}>
-        <StyledImage
+    <PostArticle>
+      <PostWrap>
+      <PostLink to={post.slug}>
+        <PostImage
           fluid={post.image.sharp.fluid}
           alt={post.title}
         />
-      </styledLink>
+      </PostLink>
       <h3>
         <Link to={post.slug}>{post.title}</Link>
       </h3>
-      <StyledP>{post.excerpt}</StyledP>
+      <PostParagraph>{post.excerpt}</PostParagraph>
       <ReadLink to={post.slug}>Read this post </ReadLink>
-      </Wrap>
-    </StyledArticle>
+      </PostWrap>
+    </PostArticle>
   </>
 )
 
