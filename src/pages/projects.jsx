0a3fcd45c3b8e2218889a669545e909362bc7579
luke-h1 <luke.howsam@yahoo.com>
2020-12-02 19:15:08 +0000

    get projects component rendering properly

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,18 +1,42 @@
-/* eslint-disable import/no-named-as-default */
-/* eslint-disable import/no-named-as-default-member */
-/* eslint-disable no-unused-vars */
 import React from 'react';
-import SEO from '../components/seo';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
+import SEO from '../components/seo';
 import Projects from '../components/Projects/Projects';
 
-const projects = ({ theme, data, location }) => {
-  const siteTitle = data.site.siteMetadata.title || 'Luke H - Projects';
+const projects = ({ data, location, theme }) => {
+  const siteTitle = data.site.siteMetadata.title || 'Luke H';
+  // const posts = data.allMarkdownRemark.nodes;
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="Projects" />
+      <SEO title="Home" />
       <Projects theme={theme} />
     </Layout>
   );
 };
+
+export default projects;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+        }
+      }
+    }
+  }
+`;
