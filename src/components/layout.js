9fbf4a1acd2ed09f84e777dfeedfc286945fab72
luke-h1 <luke.howsam@yahoo.com>
2020-12-02 17:59:41 +0000

    setup global styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,18 @@
 /* eslint-disable */
-import React from 'react';
-import { Link } from 'gatsby';
+import React from "react"
+import { Link } from "gatsby"
+import { GlobalStyle } from "../styles"
 
 const Layout = ({ location, title, children }) => {
-
-
   return (
-    <div>
-      {children}
-    </div>
-  );
-};
+    <>
+      <GlobalStyle />
 
-export default Layout;
+      <div>{children}
+      <h1>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default Layout
---
+++ src/styles/Themes.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable */
+
+import { blue, light, dark, neutral, green } from './Colors';
+import { primaryFont } from './typography';
+
+
+export const lightTheme = { 
+  primaryBackgroundColor: light[100],
+  primaryColor: dark[100],
+  darkTextColor: dark[100],
+  primaryHoverColor: blue[100],
+  primaryActiveColor: neutral[100],
+  textColorOnPrimary: neutral[100],
+  textColor: dark[100],
+  textColorInverted: neutral[400],
+  textOnDisabled: neutral[300],
+  primaryNavColor: light[100],
+  primaryFooterColor: light[100],
+  PrimaryFooterBorder: light[100],
+  primaryNavColor: light[100],
+  primaryTitleColor: dark[100],
+
+  primaryFont,
+}
+
+export const darkTheme = {
+  primaryBackgroundColor: dark[100],
+  primaryColor: neutral[100],
+  darkTextColor: dark[100],
+  primaryHoverColor: blue[100],
+  primaryActiveColor: neutral[300],
+  textColor: neutral[600],
+  textColorInverted: neutral[400],
+  textOnDisabled: neutral[300],
+  primaryNavColor: dark[100],
+  primaryFooterColor: dark[100],
+  PrimaryFooterBorder: dark[100],
+  primaryTitleColor: green[100],
+
+
+  primaryFont,
+
+}
+
