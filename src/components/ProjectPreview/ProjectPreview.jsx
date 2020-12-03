e2062dae50137880021c4de6c77bda2741546bd0
luke-h1 <luke.howsam@yahoo.com>
2020-12-03 13:43:02 +0000

    add automation project to json

--- src/components/ProjectPreview/ProjectPreview.jsx
+++ src/components/ProjectPreview/ProjectPreview.jsx
@@ -1,10 +1,20 @@
 /* eslint-disable */
 import React from 'react'
-
+import CardItem from '../../templates/CardItem/CardItem';
 
 
 const ProjectPreview = ({ Project }) => (
   <>
+  <CardItem
+    path={Project.slug}
+    data-category={Project.label}
+    fluid={Project.image.sharp.fluid} alt={Project.title}
+    text={Project.text}  
+  />
+
+
+
+
         {/* TODO: LOOP THRU & SHOW CARD ITEM FOR EACH PROJECT  */}
         
         {/* TODO: CREATE JSON OBJ TO LOOP THRU AND CREATE A PAGE BASED ON RESULTS */}
