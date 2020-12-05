00840fd1c3acd933f41cac4ab18a6bfaa5547a51
luke-h1 <luke.howsam@yahoo.com>
2020-12-05 14:15:00 +0000

    add blog post styles

--- src/templates/BlogPostItem/BlogPostItem.jsx
+++ src/templates/BlogPostItem/BlogPostItem.jsx
@@ -4,6 +4,16 @@ import {graphql} from 'gatsby';
 import styled from 'styled-components';
 import Layout from '../../components/layout';
 import {MDXRenderer} from 'gatsby-plugin-mdx';
+import {
+Blog,
+BlogLink,
+BlogLinkWrap,
+BlogPostWrapper,
+Wrapper,
+Title,
+Intro
+} from './BlogPostItemElements';
+
 export const query = graphql`
   query($slug: String!) {
     mdx(frontmatter: { slug: { eq: $slug } }) {
@@ -14,17 +24,24 @@ export const query = graphql`
     }
   }
 `
-const Styledp = styled.p`
-  font-size: 0.75rem;
-`; 
+
 
 const BlogPostItem = ({data: {mdx: post }}) => (
   <Layout>
-    <h1>{post.frontmatter.title}</h1>
-    <Styledp>posted by author</Styledp>
-        <MDXRenderer>{post.body}</MDXRenderer>
-      <p>post body goes here</p>
-      {/* <ReadLink to='/'>Back to home </ReadLink> */}
+    <BlogPostWrapper>
+      <Wrapper>
+        <Title>{post.frontmatter.title}</Title>
+        <Intro>Hello</Intro>
+        <Blog />
+        <Blog>
+        <MDXRenderer>{post.body}</MDXRenderer>     
+          </Blog>
+        <BlogLinkWrap>
+          <BlogLink to="/blog">Back to Blog</BlogLink>
+          <BlogLink to="/">Back to Home</BlogLink>
+        </BlogLinkWrap>
+      </Wrapper>
+      </BlogPostWrapper>
   </Layout>
 )
 export default BlogPostItem;
