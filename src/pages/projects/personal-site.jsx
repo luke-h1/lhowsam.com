87f26dd7bba8dcecbded096697319e61c8b08e5d
luke-h1 <luke.howsam@yahoo.com>
2020-12-05 19:03:32 +0000

    refactor blog post, update desc for website

--- src/pages/projects/personal-site.jsx
+++ src/pages/projects/personal-site.jsx
@@ -12,7 +12,7 @@ const Automation = () => (
       title="Personal Site (This one!)"
       src={Image1}
       alt="Neon sign that says: Coffee"
-      desc="This is a project I've worked on since I started learning web development. It has been through many iterations (Vanilla javascript to React, CSS to SASS and SASS to styled components to name a few...) and it has helped me challenge myself and learn a lot about web development in general. I've really enjoyed spending time improving this site and thinking of new features I can add to this site. I plan on remaking this site into a gatsby site and integrate GraphQL to simplify querying for data"
+      desc="This is a project I've worked on since I started learning web development. It has been through many iterations (Vanilla javascript to React, React to Gatsby,  CSS to SASS and SASS to styled components to name a few...) and it has helped me challenge myself and learn a lot about web development in general. I've really enjoyed spending time improving this site and thinking of new features I can add to this site."
       label="Javascript, React,CSS, SCSS, styled-components"
       github="https://github.com/luke-h1/lhowsam.com"
       site="https://lhowsam.com"
