e8ce2a8717d643ddb0f0ed6ed57ec4fce9f76d50
luke-h1 <luke.howsam@yahoo.com>
2020-12-05 16:15:10 +0000

    add experimental shell script to deploy from command line

---
+++ prod-exp.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "Are you sure your changes aren't going to break production ?"
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    CONTINUE=true
+fi
+
+if ! $CONTINUE; then
+    # Check if we're continuing and output a message if not
+    echo "Exiting with status code 1"
+    exit 1
+fi
+npm run lint 
+if ! npm run lint; then
+    echo ''
+    echo ''
+    echo "###########################################" 
+    echo "# ❌     ESLINT returned an error.     ❌  #" 
+    echo "# ❌ process exited with status code 1 ❌  #" 
+    echo "###########################################" 
+    exit 1
+else
+    echo 'eslint returned no errors. Continuing with deployment'
+fi
+npm run test 
+if ! npm run test; then 
+    echo ''
+    echo ''
+    echo "###########################################" 
+    echo "# ❌     Unit tests failed             ❌  #" 
+    echo "# ❌ process exited with status code 1 ❌  #" 
+    echo "###########################################" 
+    exit 1 
+else 
+    echo 'Unit tests have passed. Deploying to production ✅'
+fi 
+rm -rf .cache public/
+npm run build 
+vercel --prod
