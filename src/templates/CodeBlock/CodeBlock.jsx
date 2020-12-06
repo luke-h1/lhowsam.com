f296ed47bdf66e6133abb852273e748c6b67111b
luke-h1 <luke.howsam@yahoo.com>
2020-12-06 14:25:54 +0000

    add custom styles to CodeBlock

--- src/templates/CodeBlock/CodeBlock.jsx
+++ src/templates/CodeBlock/CodeBlock.jsx
@@ -2,21 +2,33 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0 20px 0;
+`;
 
 export default ({ children }) => (
-  <Highlight {...defaultProps} code={children} language="javascript">
-    {({
-      className, style, tokens, getLineProps, getTokenProps,
-    }) => (
-      <pre className={className} style={{ ...style, padding: '20px' }}>
-        {tokens.map((line, i) => (
-          <div key={i} {...getLineProps({ line, key: i })}>
-            {line.map((token, key) => (
-              <span key={key} {...getTokenProps({ token, key })} />
-            ))}
-          </div>
-        ))}
-      </pre>
-    )}
-  </Highlight>
+  <Wrap>
+    <Highlight {...defaultProps} code={children} language="javascript">
+      {({
+        className, style, tokens, getLineProps, getTokenProps,
+      }) => (
+        <pre className={className} style={{ ...style, padding: '1px' }}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  </Wrap>
 );
