de71123f773d47ab88821b1411106e861a44d2fc
luke-h1 <luke.howsam@yahoo.com>
2020-12-08 13:23:55 +0000

    fix stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-styled-components',
+    'gatsby-plugin-emotion',
 
     'gatsby-plugin-feed',
     {
