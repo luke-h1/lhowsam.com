7610a781674bff725c3b725fe9f27147382408f3
luke-h1 <luke.howsam@yahoo.com>
2020-12-08 13:04:53 +0000

    refactor codeblock

--- src/templates/CodeBlock/CodeBlock.jsx
+++ src/templates/CodeBlock/CodeBlock.jsx
@@ -5,29 +5,19 @@ import Highlight, { defaultProps } from 'prism-react-renderer';
 import styled from 'styled-components';
 import theme from 'prism-react-renderer/themes/synthwave84';
 
-const Wrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 0px 0 0px 0;
-`;
-
 export default ({ children }) => (
-  <Wrap>
-    <Highlight {...defaultProps} code={children} theme={theme} language="javascript">
-      {({
-        className, style,
-      }) => (
-        <pre
-          className={className}
-          style={{
-            ...style, padding: '8px', margin: '5px', wordBreak: 'break-word',
-          }}
-        >
-          {children}
-        </pre>
-      )}
-    </Highlight>
-  </Wrap>
+  <Highlight {...defaultProps} code={children} theme={theme} language="javascript">
+    {({
+      className, style,
+    }) => (
+      <pre
+        className={className}
+        style={{
+          ...style, padding: '8px', margin: '5px', wordBreak: 'break-word',
+        }}
+      >
+        {children}
+      </pre>
+    )}
+  </Highlight>
 );
