d9be93bdd58324ea1258289fc632845e0a660623
luke-h1 <luke.howsam@yahoo.com>
2020-12-15 08:08:41 +0000

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -29,6 +29,40 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   });
 }
 
+/* Project dynamic creation */
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panic('failed to create posts', result.errors);
+  }
+
+  const posts = result.data.allMdx.nodes;
+
+  posts.forEach(post => {
+    actions.createPage({
+      path: post.frontmatter.slug,
+      component: require.resolve('./src/templates/BlogPostItem/BlogPostItem.jsx'),
+      context: {
+        slug: post.frontmatter.slug,
+      },
+    });
+  });
+}
+
+
+
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions;
 
