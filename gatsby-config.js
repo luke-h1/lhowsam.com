60acc6eb18fe7765d15d60712116886e4cd66b87
luke-h1 <luke.howsam@yahoo.com>
2020-12-19 07:56:11 +0000

    change gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,8 +66,8 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 600,
-              quality: 80,
+              maxWidth: 500,
+              quality: 100,
             },
           },
           {
