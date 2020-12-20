0619d963762cc0b604470a63ce50ed9d0e3bb11a
luke-h1 <luke.howsam@yahoo.com>
2020-12-20 17:18:23 +0000

    feat/deploy-latest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,34 @@
 module.exports = {
   siteMetadata: {
-    title: 'Luke H',
+    title: "Luke H",
     author: {
-      name: 'Luke Howsam',
-      summary: 'My personal portfolio made with Gatsby, React & styled-components',
+      name: "Luke Howsam",
+      summary:
+        "My personal portfolio made with Gatsby, React & styled-components",
     },
-    description: 'My personal portfolio & Blog',
-    siteUrl: 'https://lhowsam.com',
+    description: "My personal portfolio & Blog",
+    siteUrl: "https://lhowsam.com",
     social: {
-      twitter: 'LukeH_1999',
+      twitter: "LukeH_1999",
     },
   },
   plugins: [
-    'gatsby-transformer-json',
+    "gatsby-transformer-json",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        path: './src/data/',
+        path: "./src/data/",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'projects',
-        path: './src/data/',
+        name: "projects",
+        path: "./src/data/",
       },
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_KEY,
         head: true,
@@ -36,68 +37,68 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-mdx',
+      resolve: "gatsby-plugin-mdx",
       options: {
         defaultLayouts: {
-          default: require.resolve('./src/components/layout.jsx'),
+          default: require.resolve("./src/components/layout.jsx"),
         },
-        gatsbyRemarkPlugins: ['gatsby-remark-images'],
-        plugins: ['gatsby-remark-images'],
+        gatsbyRemarkPlugins: ["gatsby-remark-images"],
+        plugins: ["gatsby-remark-images"],
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content/blog`,
-        name: 'blog',
+        name: "blog",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content/assets`,
-        name: 'assets',
+        name: "assets",
       },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 500,
               quality: 100,
             },
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
+            resolve: "gatsby-remark-responsive-iframe",
             options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem',
+              wrapperStyle: "margin-bottom: 1.0725rem",
             },
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
         ],
       },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-prismjs',
+            resolve: "gatsby-remark-prismjs",
             options: {
-              classPrefix: 'language-js',
-              inlineCodeMarker: { js: 'javascript' },
+              classPrefix: "language-js",
+              inlineCodeMarker: { js: "javascript" },
               aliases: {},
               showLineNumbers: true,
               noInlineHighlight: true,
               languageExtensions: [
                 {
-                  language: 'superscript',
-                  extend: 'javascript',
+                  language: "superscript",
+                  extend: "javascript",
                   definition: {
                     superscript_types: /(SuperType)/,
                   },
@@ -109,8 +110,8 @@ module.exports = {
                 },
               ],
               prompt: {
-                user: 'root',
-                host: 'localhost',
+                user: "root",
+                host: "localhost",
                 global: false,
               },
               escapeEntities: {},
@@ -119,24 +120,24 @@ module.exports = {
         ],
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-styled-components',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-styled-components",
 
-    'gatsby-plugin-feed',
+    "gatsby-plugin-feed",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'luke howsam',
-        short_name: 'lhowsam.com',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'content/assets/logo192.png',
+        name: "luke howsam",
+        short_name: "lhowsam.com",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "content/assets/logo192.png",
       },
     },
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     // `gatsby-plugin-offline`,
   ],
-};
+}
