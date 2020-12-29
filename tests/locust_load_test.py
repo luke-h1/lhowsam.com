031262108a3d74840bb5b2aec9a9b2cdabdee5c3
luke-h1 <luke.howsam@yahoo.com>
2020-12-29 18:25:01 +0000

    fix typo in load tests

--- tests/locust_load_test.py
+++ tests/locust_load_test.py
@@ -4,10 +4,10 @@ from locust import HttpUser, task
 class QuickstartUser(HttpUser):
     @task
     def hello_world(self):
-        self.client.get("/")
-        self.client.get("/projects")
-        self.client.get("/contact")
-        self.client.get("/blog")
-        self.client.get("/about")
+        self.client.get("")
+        self.client.get("projects")
+        self.client.get("contact")
+        self.client.get("blog")
+        self.client.get("about")
 
 
