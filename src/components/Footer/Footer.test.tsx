1f47134a82ff8b03d0b4d675d039882f504f9a4b
luke-h1 <luke.howsam@yahoo.com>
2021-01-03 20:58:09 +0000

    try to fix footer test

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,10 +1,14 @@
+/* eslint-disable */
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Footer from './Footer';
 import { theme } from '../../styles/Themes';
 
+
 test('Footer matches snapshot', () => {
-  const footer = renderer.create(<Footer theme={theme} />)
+  const footer = renderer.create(<Footer theme={theme}/>)
+
+
     .toJSON();
   expect(footer).toMatchSnapshot();
 });
