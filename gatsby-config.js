843b73fd30353f3df42b7f95aeddb03e425cd1b4
luke-h1 <luke.howsam@yahoo.com>
2021-01-16 15:03:23 +0000

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,143 +1,32 @@
-const { GOOGLE_ANALYTICS_KEY } = process.env;
-
 module.exports = {
   siteMetadata: {
-    title: 'lhowsam',
-    author: {
-      name: 'Luke howsam',
-      summary: 'who lives and works in Sheffield.',
-    },
-    description:
-      'My personal portfolio built with Gatsby & styled components :) ',
-    siteUrl: 'https://lhowsam.com',
-    social: {
-      twitter: 'lukeH_1999',
-    },
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
-        name: 'blog',
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
-      resolve: 'gatsby-plugin-mdx',
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        defaultLayouts: {
-          default: require.resolve('./src/components/layout.tsx'),
-        },
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-theme-ui',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'projects',
-        path: './content/project',
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: 'assets',
-      },
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-prismjs',
-            options: {
-              classPrefix: 'language-',
-              inlineCodeMarker: null,
-              aliases: {},
-              showLineNumbers: false,
-              noInlineHighlight: false,
-              languageExtensions: [
-                {
-                  language: 'superscript',
-                  extend: 'javascript',
-                  definition: {
-                    superscript_types: /(SuperType)/,
-                  },
-                  insertBefore: {
-                    function: {
-                      superscript_keywords: /(superif|superelse)/,
-                    },
-                  },
-                },
-              ],
-              prompt: {
-                user: 'root',
-                host: 'localhost',
-                global: false,
-              },
-              escapeEntities: {},
-            },
-          },
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-canonical-urls',
-      options: {
-        siteUrl: 'https://lhowsam.com',
-        stripQueryString: true,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-typescript',
-    },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 630,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem',
-            },
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-        ],
-      },
-    },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-styled-components',
-
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: `${GOOGLE_ANALYTICS_KEY}`,
-      },
-    },
-    'gatsby-plugin-feed',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'lhowsam.com',
-        short_name: 'lhowsam.com',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#000',
-        display: 'minimal-ui',
-        icon: 'content/assets/logo512.png',
-      },
-    },
-    'gatsby-plugin-react-helmet',
     // `gatsby-plugin-offline`,
   ],
-};
+}
