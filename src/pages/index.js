92eb8f9b6b0df495926c7634d712a13e864f17f8
luke-h1 <luke.howsam@yahoo.com>
2021-01-16 15:11:30 +0000

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import Intro from '../components/Intro/Intro';
+import { theme } from '../styles/Theme';
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title='Home' />
+    <Intro theme={theme} />
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
