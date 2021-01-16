8f13c63252be484076dfc53778e96aa55a9cbdb4
luke-h1 <luke.howsam@yahoo.com>
2021-01-16 19:36:02 +0000

    get usePosts hook working"

--- src/components/BlogPage/BlogPage.tsx
+++ src/components/BlogPage/BlogPage.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 import BlogItem from '../BlogItem/BlogItem';
 import { ThemeProvider } from 'styled-components';
 import { Wrapper, Intro, Title } from './BlogElements';
-
+import usePosts from '../../hooks/usePosts';
 const BlogPage = ({ theme }) => {
+  const posts = usePosts();
   return (
     <ThemeProvider theme={theme}>
       <>
         <Wrapper>
-            <Title>Blog</Title>
-          <Intro>
-          </Intro>
-          <BlogItem />
-          <BlogItem />
-          <BlogItem />
-          <BlogItem />
+          <Title>Blog</Title>
+          <Intro></Intro>
+          {posts.map(post => {
+            <h1>{post.title}</h1>
+          })}
         </Wrapper>
       </>
     </ThemeProvider>
