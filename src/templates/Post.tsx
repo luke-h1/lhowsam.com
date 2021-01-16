9de4063d4dceb3339a1710c02de82396edfeef2d
luke-h1 <luke.howsam@yahoo.com>
2021-01-16 19:55:06 +0000

    add style improvements to Posts.tsx

--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -18,13 +19,42 @@ export const query = graphql`
   }
 `;
 
+const PostWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 25vh;
+  margin-bottom: 4rem;
+`;
+
+const PostTitle = styled.h1`
+  color: ${props => props.theme.darkTextColor};
+`;
+
+const ContentWrapper = styled.div`
+  width: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Small = styled.p`
+  color: ${(props) => props.theme.greyTextColor};
+  text-align: center;
+  margin-bottom: 4rem;
+`
+
 const Post = ({ data: { mdx: post } }) => {
   return (
     <Layout>
       <SEO title={post.frontmatter.title} description={post.frontmatter.description || post.excerpt} />
-      <p>
-        <MDXRenderer>{post.body}</MDXRenderer>
-      </p>
+      <PostWrapper>
+        <PostTitle>{post.frontmatter.title}</PostTitle>
+        <Small>{post.frontmatter.date}</Small>
+        <ContentWrapper><MDXRenderer>{post.body}</MDXRenderer></ContentWrapper>
+      </PostWrapper>
     </Layout>
   );
 };
