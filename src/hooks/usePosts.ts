dc1c689583a5eb14d01fa6480bcb174589991e79
luke-h1 <luke.howsam@yahoo.com>
2021-01-17 13:11:51 +0000

    format date for all gql queries & add ts as dev dep

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,32 +1,29 @@
 import { graphql, useStaticQuery } from 'gatsby';
 
-
 const usePosts = () => {
   const data = useStaticQuery(graphql`
-query getPosts {
-  allMdx(filter: {fields: {slug: {ne: "/__do-not-remove/"}}}) {
-    nodes {
-      fields {
-        slug
-      }
-      frontmatter {
-        date
-        excerpt
-        tags
-        title
+    query getPosts {
+      allMdx(filter: { fields: { slug: { ne: "/__do-not-remove/" } } }) {
+        nodes {
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "DD/MM/YYYY")
+            excerpt
+            tags
+            title
+          }
+        }
       }
     }
-  }
-}
-
-
   `);
-  return data.allMdx.nodes.map((post) => ({
+  return data.allMdx.nodes.map(post => ({
     slug: post.fields.slug,
     date: post.frontmatter.date,
     excerpt: post.frontmatter.excerpt,
     tags: post.frontmatter.tags,
-    title: post.frontmatter.title
-  }))
+    title: post.frontmatter.title,
+  }));
 };
 export default usePosts;
