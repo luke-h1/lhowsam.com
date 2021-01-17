7193db1653c3fbd104c22bd919a4b15414f6f816
luke-h1 <luke.howsam@yahoo.com>
2021-01-17 16:05:34 +0000

    updates

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,61 +1,60 @@
-const path = require("path")
-const { createFilePath } = require("gatsby-source-filesystem")
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
   if (node.internal.type === `Mdx`) {
     // Create slug
-    const slug = createFilePath({ node, getNode, basePath: `` })
+    const slug = createFilePath({ node, getNode, basePath: `` });
     createNodeField({
       node,
       name: `slug`,
       value: slug,
-    })
+    });
   }
-}
+};
 
 exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
-  const postTemplate = path.resolve(
-    "src/templates/Post.tsx"
-  )
+  const postTemplate = path.resolve('src/templates/Post.tsx');
+
+  const projectTemplate = path.resolve('src/templates/Project.tsx');
 
   return graphql(`
-  query MyQuery {
-    allMdx(sort: {fields: frontmatter___date}) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-          frontmatter {
-            date
-            draft
-            excerpt
-            tags
-            title
+    query MyQuery {
+      allMdx(sort: { fields: frontmatter___date }) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              date
+              draft
+              excerpt
+              tags
+              title
+            }
           }
         }
       }
     }
-  }
-  
   `).then(res => {
     if (res.errors) {
-      return Promise.reject(res.errors)
+      return Promise.reject(res.errors);
     }
     // console.log(JSON.stringify(res, null, 4)) ///
 
     // Create pages & register paths
-    const edges = res.data.allMdx.edges
+    const edges = res.data.allMdx.edges;
     edges.forEach((edge, i) => {
-      const node = edge.node
+      const node = edge.node;
 
-      const prev = getPrevAvailableNode(edges, i + 1)
-      const next = getNextAvailableNode(edges, i - 1)
+      const prev = getPrevAvailableNode(edges, i + 1);
+      const next = getNextAvailableNode(edges, i - 1);
 
-      if (node.fields.slug !== "/__do-not-remove/") {
+      if (node.fields.slug !== '/__do-not-remove/') {
         createPage({
           path: node.fields.slug,
           component: postTemplate,
@@ -64,50 +63,64 @@ exports.createPages = ({ actions, graphql }) => {
             next,
             prev,
           },
-        })
+        });
       }
-    })
-  })
-}
+    });
+  });
+  // query MyQuery {
+  //   allProjectsJson {
+  //     edges {
+  //       node {
+  //         body
+  //         excerpt
+  //         id
+  //         slug
+  //         title
+  //       }
+  //     }
+  //   }
+  // }
+  
+};
 
 // Get next available prev node that's not about, draft, and dummy post
 const getPrevAvailableNode = (edges, index) => {
-  let retVal
+  let retVal;
 
   for (let i = index; i < edges.length - 1; i++) {
     if (!skipNode(edges[i].node)) {
-      retVal = edges[i].node
-      break
+      retVal = edges[i].node;
+      break;
     }
   }
-  return retVal
-}
+  return retVal;
+};
 
 const getNextAvailableNode = (edges, index) => {
-  let retVal
+  let retVal;
 
   for (let i = index; i > 0; i--) {
     if (!skipNode(edges[i].node)) {
-      retVal = edges[i].node
-      break
+      retVal = edges[i].node;
+      break;
     }
   }
-  return retVal
-}
+  return retVal;
+};
 
 // Skip node if it's about, draft, or dummy post
 const skipNode = node => {
-  return isAboutPage(node) || isDraft(node) || isDummy(node)
-}
+  return isAboutPage(node) || isDraft(node) || isDummy(node);
+};
 
 const isAboutPage = node => {
-  return node.fields.slug === "/about/"
-}
+  return node.fields.slug === '/about/';
+};
 
 const isDraft = node => {
-  return node.frontmatter.draft === true
-}
+  return node.frontmatter.draft === true;
+};
 
 const isDummy = node => {
-  return node.frontmatter.tags && node.frontmatter.tags.includes("___dummy*")
-}
+  return node.frontmatter.tags && node.frontmatter.tags.includes('___dummy*');
+};
