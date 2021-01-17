104babbea9c3ccd1ca6642489d368b25dd40e8ed
luke-h1 <luke.howsam@yahoo.com>
2021-01-17 17:21:55 +0000

    updates

--- config/website.js
+++ config/website.js
@@ -1,18 +1,20 @@
-module.exports =  {
-    pathPrefix: '/',
-    title: 'luke howsam - personal portfolio',
-    titleAlt: 'luke howsam personal portfolio',
-    description: 'My personal portfolio made with Gatsby, Graphql, jest, cypress, styled-components & MDX',
-    headline: '',
-    url: 'https://lhowsam.com',
-    siteLanguage: 'en',
-    logo: '/src/images/logo512.png',
-    ogLanguage: 'en',
-    // JSONLD / MANIFEST 
-    favicon: '/src/images/favicon.ico',
-    shortName: 'lhowsam',
-    author: 'luke howsam',
-    themeColor: '#ffffff',
-    backgroundColor: '#000',
-    twitter: 'LukeH_1999',
-}
+module.exports = {
+  pathPrefix: '/',
+  title: 'luke howsam - personal portfolio',
+  titleAlt: 'luke howsam personal portfolio',
+  description: 'My personal portfolio made with Gatsby, Graphql, jest, cypress, styled-components & MDX',
+  headline: '',
+  url: 'https://lhowsam.com',
+  siteLanguage: 'en',
+  logo: '/src/images/logo512.png',
+  disqusShortname: 'https://lhowsam.com', // Disqus shortname.
+
+  ogLanguage: 'en',
+  // JSONLD / MANIFEST
+  favicon: '/src/images/favicon.ico',
+  shortName: 'lhowsam',
+  author: 'luke howsam',
+  themeColor: '#ffffff',
+  backgroundColor: '#000',
+  twitter: 'LukeH_1999',
+};
