621aa4d94a79a8a763096b6f17545cb23a790f3d
luke-h1 <luke.howsam@yahoo.com>
2021-01-17 15:09:02 +0000

    fix UI tests

--- cypress/e2e/home.js
+++ cypress/e2e/home.js
@@ -1,21 +1,21 @@
+/// <reference types="Cypress" />
+
 describe('home', () => {
-    beforeEach(() => {
-      cy.visit('/').waitForRouteChange()
-    })
-  
-    it('should have the hero content', () => {
-      cy.findAllByText(/hey I'm Luke/i)
-      cy.findByText(/I'm passionate about solving technical problems and coming up with creative solutions.I enjoy working with the JAMstack, React, Node, testing technologies + practices & various dev-ops technologies/i)
-    })
-  
-    it('should have recent projects', () => {
-      cy.findByText(/Projects/i)
-    })
-  
-    it('should have recent blog posts', () => {
-      cy.findByText(/forcing git merges/i)
+  beforeEach(() => {
+    cy.visit('/').waitForRouteChange();
+  });
 
-    })
+  it('should have hero content', () => {
+    cy.findAllByText(/I'm Luke/i);
+    cy.findAllByText(
+      /I'm passionate about solving technical problems and coming up with creative solutions.I enjoy working with the JAMstack, React, Node, testing technologies + practices & various dev-ops technologies/i,
+    );
+  });
+  it('should have recent projects', () => {
+    cy.findByText(/Projects/i);
+  });
 
-  })
-  
+  it('should have recent blog posts', () => {
+    cy.findByText(/forcing git merges/i);
+  });
+});
