76e74440718a3ffb7c906ad998ada7a9ca99c36f
luke-h1 <luke.howsam@yahoo.com>
2021-01-17 16:25:22 +0000

    updates

--- src/components/ProjectPage/ProjectPage.tsx
+++ src/components/ProjectPage/ProjectPage.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import ProjectItem from '../ProjectItem/ProjectItem';
 import { Wrapper, Title, Intro } from './ProjectPageElements';
+import useProjects from '../../hooks/useProjects';
 
-const ProjectPage = ({ theme }) => (
-  <>
-    <ThemeProvider theme={theme}>
-      <Wrapper>
-        <Title>Projects</Title>
-        <Intro />
-        <ProjectItem />
-        <ProjectItem />
-        <ProjectItem />
-        <ProjectItem />
-      </Wrapper>
-    </ThemeProvider>
-  </>
-);
+const ProjectPage = ({ theme }) => {
+  const projects = useProjects()
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Wrapper>
+          <Title>Projects</Title>
+          <Intro />
+          {projects.map(project => (
+            <ProjectItem title={project.title} excerpt={project.excerpt} />
+          ))}
+        </Wrapper>
+      </ThemeProvider>
+    </>
+  );
+};
 export default ProjectPage;
