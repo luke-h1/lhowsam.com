be97b48558ed596dbf8a3519a57ef0acac9f706c
luke-h1 <luke.howsam@yahoo.com>
2021-01-20 20:31:31 +0000

    add share buttons

---
+++ components/ShareButtons/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  EmailShareButton, TwitterShareButton, LinkedinShareButton, EmailIcon, TwitterIcon, LinkedinIcon,
+} from 'react-share';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ShareBtnWrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 0.2rem 0.4rem;
+
+  * {
+    margin: 0 0.2rem;
+  }
+`;
+
+const ShareButtons: React.FC<{location: string}> = ({ location }) => (
+  <ShareBtnWrap>
+    <EmailShareButton url={location}>
+      <EmailIcon round size={35} />
+    </EmailShareButton>
+
+    <TwitterShareButton url={location}>
+      <TwitterIcon round size={35} />
+    </TwitterShareButton>
+
+    <LinkedinShareButton url={location}>
+      <LinkedinIcon round size={35} />
+    </LinkedinShareButton>
+  </ShareBtnWrap>
+);
+
+ShareButtons.propTypes = {
+  location: PropTypes.string.isRequired,
+};
+
+export default ShareButtons;
