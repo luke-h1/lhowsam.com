b70c0ef853075cd51da2aae75d430a8892fb8539
luke-h1 <luke.howsam@yahoo.com>
2021-01-21 13:11:01 +0000

    add sitemap script + other deps

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
-const withMDX = require('@next/mdx')();
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/u
+});
 
 module.exports = withMDX({
   pageExtensions: ['js', 'mdx'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./scripts/generate-sitemap');
+    }
+
+    return config;
+  }
 });
