4daaad952f90f60416a0fa4918b3ec451f0e000f
luke-h1 <luke.howsam@yahoo.com>
2021-01-23 14:29:19 +0000

    fix font situation for now (hacky)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import { GlobalStyle } from "../styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../styles/Theme";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
