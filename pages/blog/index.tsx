b8838426bd9b37ccf926dcba7d3e787b55ac96fe
luke-h1 <luke.howsam@yahoo.com>
2021-01-23 15:44:33 +0000

    move BlogPage to root

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,13 +1,34 @@
 import React from "react";
-import BlogPage from '../../layout/BlogPage/BlogPage';
 import { ThemeProvider } from "styled-components";
 import { theme } from "../../styles/Theme";
+
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 40vh;
+  min-width: 100vw;
+`;
+
+const Intro = styled.p``;
+
+const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 5rem;
+`;
 
 export default function Index() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <BlogPage theme={theme} />
+        <Wrapper>
+          <Title>Blog root page</Title>
+          <Intro />
+          {/* map thru blog posts here */}
+        </Wrapper>
       </ThemeProvider>
     </>
   );
