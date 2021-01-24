a7c1cdea4811b123cc2b8d1c934fa55988a20d07
luke-h1 <luke.howsam@yahoo.com>
2021-01-24 11:33:32 +0000

    update eslint ignore rules & start to add propTypes to everything

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+
+import { ThemeProvider } from 'styled-components';
 import {
   NavWrapper,
   NavbarContainer,
@@ -10,59 +13,59 @@ import {
   NavItem,
   NavItemBtn,
   NavLinks,
-  NavBtnLink,
-} from "./NavStyles";
-
-interface NavInt {
-  title: string;
-  slug: string;
-}
+} from './NavStyles';
 
 const Nav: React.FC<{ theme: String }> = ({ theme }) => {
-  const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
+  const [click, setClick]: any = useState(false);
+  const handleClick: any = () => setClick(!click);
+  const closeMobileMenu: any = () => setClick(false);
 
   return (
     <>
-      <NavWrapper>
-        <NavbarContainer>
-          <NavLogo href="/" onClick={closeMobileMenu}>
-            <a>
-              <NavIcon />
-            </a>
-          </NavLogo>
-          <MobileIcon onClick={handleClick}>
-            {click ? <FaTimes /> : <FaBars />}
-          </MobileIcon>
-          <NavMenu onClick={handleClick} click={click}>
-            <NavItem>
-              <NavLinks href="/" onClick={closeMobileMenu}>
-                <a>Home</a>
-              </NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks href="/about" onClick={closeMobileMenu}>
-                <a>About</a>
-              </NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks href="/projects" onClick={closeMobileMenu}>
-                <a>Projects</a>
-              </NavLinks>
-            </NavItem>
-            <NavItem>
-              <NavLinks href="/blog" onClick={closeMobileMenu}>
-                <a>Blog</a>
-              </NavLinks>
-            </NavItem>
-            <NavItemBtn>{/* <NavBtnLink href='test'/> */}</NavItemBtn>
-            <NavItemBtn />
-          </NavMenu>
-        </NavbarContainer>
-      </NavWrapper>
+      <ThemeProvider theme={theme}>
+        <NavWrapper>
+          <NavbarContainer>
+            <NavLogo href="/" onClick={closeMobileMenu}>
+              <a>
+                <NavIcon />
+              </a>
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem>
+                <NavLinks href="/" onClick={closeMobileMenu}>
+                  <a>Home</a>
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks href="/about" onClick={closeMobileMenu}>
+                  <a>About</a>
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks href="/projects" onClick={closeMobileMenu}>
+                  <a>Projects</a>
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks href="/blog" onClick={closeMobileMenu}>
+                  <a>Blog</a>
+                </NavLinks>
+              </NavItem>
+              <NavItemBtn>{/* <NavBtnLink href='test'/> */}</NavItemBtn>
+              <NavItemBtn />
+            </NavMenu>
+          </NavbarContainer>
+        </NavWrapper>
+      </ThemeProvider>
     </>
   );
+};
+
+Nav.propTypes = {
+  theme: PropTypes.string.isRequired,
 };
 
 export default Nav;
