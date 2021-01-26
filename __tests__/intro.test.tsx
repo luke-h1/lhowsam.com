dc4aae978133b9e05fdcf0bcb320a732c50351d0
luke-h1 <luke.howsam@yahoo.com>
2021-01-26 20:09:38 +0000

    add intro test

---
+++ __tests__/intro.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import Intro from '../components/Intro/Intro';
+import { theme } from '../styles/Theme';
+
+afterEach(cleanup);
+
+test('intro component has correct text + renders correctly', () => {
+  const { getByTestId } = render(<Intro theme={theme} />);
+  const paragraph = getByTestId('intro');
+  expect(paragraph.textContent).toEqual('I\'m passionate about solving technical problems and coming up with creative solutions.I enjoy working with the JAMstack, React, Node, testing technologies + practices & various dev-ops technologies');
+});
--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { IntroWrapper, HighLight, DescWrap, Title } from './IntroStyles';
+import {
+  IntroWrapper, HighLight, DescWrap, Title,
+} from './IntroStyles';
 
 const Intro = ({ theme }) => (
   <ThemeProvider theme={theme}>
     <>
       <IntroWrapper>
         <Title>
-          Hi, I'm <HighLight>Luke</HighLight>
+          Hi, I'm
+          {' '}
+          <HighLight>Luke</HighLight>
         </Title>
         <DescWrap>
-          <p>
+          <p data-testid="intro">
             I'm passionate about solving technical problems and coming up with
             creative solutions.I enjoy working with the JAMstack, React, Node,
             testing technologies + practices & various dev-ops technologies
