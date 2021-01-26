196c39abca328e34d81a9fdf101babdba33ed873
luke-h1 <luke.howsam@yahoo.com>
2021-01-26 20:05:36 +0000

    finish nav test

--- __tests__/nav.test.tsx
+++ __tests__/nav.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import Nav from '../components/Nav/Nav';
 import { theme } from '../styles/Theme';
 
