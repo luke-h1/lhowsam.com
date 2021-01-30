fc849f0a57e4f7103d04843b7b53343fce751602
luke-h1 <luke.howsam@yahoo.com>
2021-01-30 11:55:39 +0000

    setup _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Layout from '../components/Layout/Layout';
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <Layout>
