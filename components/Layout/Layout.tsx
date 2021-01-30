348448df72b943fe049a82283222f718c6cac6f5
luke-h1 <luke.howsam@yahoo.com>
2021-01-30 12:16:46 +0000

    fix build error

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -20,7 +20,7 @@ const Layout = ({ children }) => {
               rel="stylesheet"
             />
           </Head>
-          <Nav theme={theme} />
+          <Nav />
           {children}
           <Footer theme={theme} />
         </MDXProvider>
