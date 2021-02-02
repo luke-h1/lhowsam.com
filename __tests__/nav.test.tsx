67d85a92106c1434ad6f113b68cf36ce8b61aa28
luke-h1 <luke.howsam@yahoo.com>
2021-02-02 19:41:28 +0000

    remove old nav bar & add absolute importers to unit tests

--- __tests__/nav.test.tsx
+++ __tests__/nav.test.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable */
-import NavData from '@components/NavBar/data';
+import data from '../components/NavBar/data.json'
 
 
 test('Navbar data is correct', () => { 
-  expect(NavData).toHaveLength(4);
-  expect(NavData.map((i) => i.name)).toEqual([
+  expect(data).toHaveLength(4);
+  expect(data.map((i) => i.name)).toEqual([
     'Home',
     'About',
     'Projects',
@@ -13,21 +13,21 @@ test('Navbar data is correct', () => {
   ]); 
 }); 
 
-for (let i = 0; i < NavData.length; i += 1){
-  test('NavData[${i}] should have keys id, name & slug', () => { 
-    expect(NavData[i]).toHaveProperty('id');
-    expect(NavData[i]).toHaveProperty('name');
-    expect(NavData[i]).toHaveProperty('slug');
+for (let i = 0; i < data.length; i += 1){
+  test('data[${i}] should have keys id, name & slug', () => { 
+    expect(data[i]).toHaveProperty('id');
+    expect(data[i]).toHaveProperty('name');
+    expect(data[i]).toHaveProperty('slug');
 
   })
 }
 
-test('NavData returns Contact as last item', () => { 
-  const Item1 = NavData[0];
-  const Item2 = NavData[1];
-  const Item3 = NavData[2];
-  const Item4 = NavData[3];
-  const Item5 = NavData[4];
+test('data returns Contact as last item', () => { 
+  const Item1 = data[0];
+  const Item2 = data[1];
+  const Item3 = data[2];
+  const Item4 = data[3];
+  const Item5 = data[4];
   const i = jest.fn((currentName) => currentName.name);
   i(Item1); // home 
   i(Item2); // about 
