5760c370a3d76d9ea8a0be8e263d3c2061caf414
luke-h1 <luke.howsam@yahoo.com>
2021-02-13 16:29:58 +0000

    refactor post & projectPost.tsx to use tailwind css

--- templates/ProjectPost.tsx
+++ templates/ProjectPost.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import { FunctionComponent } from 'react';
 import { NextSeo } from 'next-seo';
 import ShareButtons from '@components/ShareButtons';
 import Github from '@icons/Github';
@@ -24,18 +24,17 @@ const ProjectPost: FunctionComponent<Iprops> = ({ children, frontMatter }) => {
         <h1 className="text-2xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           {frontMatter.title}
         </h1>
-        <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2 mb-8 prose dark:prose-dark max-w-none w-full">
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2 mb-8">
           <div className="flex items-center">
             {frontMatter.github && <Github url={frontMatter.github} />}
             {frontMatter.site && <Site url={frontMatter.site} />}
           </div>
           <p className="text-sm text-gray-500 min-w-32 mt-2 md:mt-0">
+            {' • '}
             {frontMatter.readingTime.text}
           </p>
         </div>
-        <div className="prose dark:prose-dark max-w-none w-full">
-          <p className="text-gray-600 dark:text-gray-400">{children}</p>
-        </div>
+        <div className="prose  max-w-none w-full mt-4 mb-4">{children}</div>
 
         <div className="text-sm text-gray-700 dark:text-gray-300 flex flex-col align-center justify-center mx-auto py-5">
           <ShareButtons
