f49d36825844c2dfc41d9d7ab16c911379651a62
luke-h1 <luke.howsam@yahoo.com>
2021-02-15 19:46:18 +0000

    fix padding

--- templates/ProjectPost.tsx
+++ templates/ProjectPost.tsx
@@ -27,19 +27,20 @@ const ProjectPost: FunctionComponent<Iprops> = ({ children, frontMatter }) => {
           <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
             {frontMatter.title}
           </h1>
-          <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2 mb-">
+          <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2 mb-10">
             <ShareButtons
               location={`https://lhowsam.com/projects/${frontMatter.slug}`}
             />
 
             <div className="flex items-center">
               <p className="text-sm text-gray-700 dark:text-gray-300 flex mt-5">
+
                 {frontMatter.github && <Github url={frontMatter.github} />}
                 {frontMatter.site && <Site url={frontMatter.site} />}
               </p>
             </div>
 
-            <p className="text-sm text-gray-500 min-w-32 mb-4 mt-2 md:mt-0">
+            <p className="text-sm text-gray-500 min-w-32 mt-2 md:mt-0">
               {' • '}
               {frontMatter.readingTime.text}
             </p>
