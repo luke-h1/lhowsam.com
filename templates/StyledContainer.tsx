13aad2720abd83cee6377c45c0a30843f1af9629
luke-h1 <luke.howsam@yahoo.com>
2021-02-16 12:21:46 +0000

    increase line-height of blog & project posts

--- templates/StyledContainer.tsx
+++ templates/StyledContainer.tsx
@@ -1,6 +1,6 @@
 const StyledContainer = ({ children }) => {
   return (
-    <div className="flex flex-col justify-center bg-white dark:bg-black px-8">
+    <div className="flex flex-col justify-center bg-white dark:bg-black px-8 leading-8">
       {children}
     </div>
   );
