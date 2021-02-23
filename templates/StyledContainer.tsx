610e41c01e67a141adc7a2af9a97cf0071b111b5
luke-h1 <luke.howsam@yahoo.com>
2021-02-23 18:56:36 +0000

    delete more files

--- templates/StyledContainer.tsx
+++
@@ -1,8 +0,0 @@
-const StyledContainer = ({ children }) => {
-  return (
-    <div className="flex flex-col justify-center bg-white dark:bg-black px-8 leading-8">
-      {children}
-    </div>
-  );
-};
-export default StyledContainer;
