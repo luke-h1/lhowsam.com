ed094ff7cb38f940a979849bbe5a4af419486c1f
luke-h1 <luke.howsam@yahoo.com>
2021-02-23 19:11:33 +0000

    add in new footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,77 +2,77 @@
 import {
   FiGithub, FiTwitter, FiMail, FiLinkedin,
 } from 'react-icons/fi';
-import { ThemeProvider } from '@emotion/react';
-
 import {
-  FooterContainer,
-  FooterWrap,
-  SocialMedia,
-  SocialMediaWrap,
-  SocialIconLink,
-  SocialIcons,
-  LinkWrapper,
-  LinkItem,
-} from './FooterStyles';
+  Flex, IconButton,
+} from '@chakra-ui/react';
 
-const Footer = ({ theme }) => {
+const Footer = () => {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <FooterContainer>
-          <FooterWrap>
-            <SocialMedia>
-              <SocialMediaWrap>
-                <SocialIcons>
-                  <SocialIconLink
-                    href="https://github.com/luke-h1"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FiGithub />
-                  </SocialIconLink>
-                  <SocialIconLink
-                    href="https://www.linkedin.com/in/lukehowsam/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FiLinkedin />
-                  </SocialIconLink>
-                  <SocialIconLink
-                    href="mailto:luke.howsam@yahoo.com?subject=Get In Touch"
-                    aria-label="Email"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FiMail />
-                  </SocialIconLink>
-                  <SocialIconLink
-                    href="https://twitter.com/LukeH_1999"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FiTwitter />
-                  </SocialIconLink>
-                </SocialIcons>
-              </SocialMediaWrap>
-            </SocialMedia>
-            <LinkWrapper>
-              <LinkItem href="/">
-                <a>/Home</a>
-              </LinkItem>
-              <LinkItem href="/about">
-                <a>/About</a>
-              </LinkItem>
-              <LinkItem href="/projects">
-                <a>/Projects</a>
-              </LinkItem>
-              <LinkItem href="/blog">
-                <a>/Blog</a>
-              </LinkItem>
-            </LinkWrapper>
-          </FooterWrap>
-        </FooterContainer>
-      </ThemeProvider>
+      <Flex align="center" mb={4} direction="column" display={['none', 'flex', 'flex']}>
+        <div>
+          <a
+            href="https://twitter.com/lukeH_1999"
+            title="Twitter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconButton
+              aria-label="Twitter"
+              icon={<FiTwitter />}
+              size="lg"
+              color="black"
+              variant="ghost"
+              _hover={{ color: 'blue' }}
+            />
+          </a>
+          <a
+            href="https://github.com/luke-h1"
+            title="GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconButton
+              aria-label="GitHub"
+              icon={<FiGithub />}
+              size="lg"
+              color="black"
+              variant="ghost"
+              _hover={{ color: 'blue' }}
+            />
+          </a>
+          <a
+            href="mailto:luke.howsam@yahoo.com?subject=Get In Touch"
+            title="Email"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconButton
+              aria-label="Email"
+              icon={<FiMail />}
+              size="lg"
+              color="black"
+              variant="ghost"
+              _hover={{ color: 'blue' }}
+            />
+          </a>
+          <a
+            href="linkedin"
+            title="Linkedin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconButton
+              aria-label="Linkedin"
+              icon={<FiLinkedin />}
+              size="lg"
+              color="black"
+              variant="ghost"
+              _hover={{ color: 'blue' }}
+            />
+          </a>
+        </div>
+      </Flex>
     </>
   );
 };
