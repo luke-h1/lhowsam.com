d29a2a34830d5253a704f58b2a09dbe081a5c3ee
luke-h1 <luke.howsam@yahoo.com>
2021-02-23 17:50:24 +0000

    add null for props

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,27 +1,19 @@
 import { MDXProvider } from '@mdx-js/react';
-import { ThemeProvider } from '@emotion/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Reset } from '@styles/reset';
 import { theme } from '@styles/Theme';
 
 import MDXComponents from '@components/MDXComponents';
 import Footer from '@components/Footer/Footer';
-import Header from '../components/Header/Header';
-// import Sidebar from '@components/SideBar/SideBar';
-// import NavBar from '@components/NavBar/NavBar';
+import Header from './Header/Header';
 
 const Layout = ({ children }) => {
-  // const [isOpen, setIsOpen] = useState(false);
-  // const toggle = () => {
-  //   setIsOpen(!isOpen);
-  // };
-
   return (
     <>
       {Reset}
       <ChakraProvider>
         <MDXProvider components={MDXComponents}>
-          <Header />
+          <Header props={null} />
           {children}
           <Footer theme={theme} />
         </MDXProvider>
