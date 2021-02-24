8f20c4b9c64a8be548b41a1875e22ca97774142a
luke-h1 <luke.howsam@yahoo.com>
2021-02-24 11:39:21 +0000

    update sitemap

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,20 +1,24 @@
 import { MDXProvider } from '@mdx-js/react';
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import {
+  ChakraProvider, theme,
+} from '@chakra-ui/react';
 import { Reset } from '@styles/reset';
 import MDXComponents from '@components/MDXComponents';
 import Footer from '@components/Footer';
 import Header from './Header/Header';
+import { prismLightTheme, prismDarkTheme } from '../styles/prism'
 
 const Layout = ({ children }) => {
   return (
     <>
       {Reset}
-      <ChakraProvider theme={theme}>
+      <ChakraProvider resetCSS theme={theme}>
         <MDXProvider components={MDXComponents}>
           <Header props={null} />
           {children}
           <Footer />
         </MDXProvider>
+
       </ChakraProvider>
     </>
   );
--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { NextSeo } from 'next-seo';
 import Image from 'next/image';
 import { NextPage } from 'next';
-import { Flex, SimpleGrid, Box, Text, Container } from '@chakra-ui/react';
+import {
+  Flex, SimpleGrid, Box, Text, Container,
+} from '@chakra-ui/react';
 
 const About: NextPage = () => {
   return (
