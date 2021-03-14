1985cad588035088dc1e108744676dcabb20fa6f
luke-h1 <luke.howsam@yahoo.com>
2021-03-14 15:24:45 +0000

    feat/improve-mdx-posts

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -2,43 +2,43 @@ import { Heading, Text } from '@chakra-ui/react';
 import React from 'react';
 
 export const H1 = ({ children }: { children: React.ReactNode }) => (
-  <Heading as="h1" size="xl" pb="1rem">
+  <Heading as="h1" size="xl" pb="1rem" pt="1rem" mt="1rem" mb="1rem">
     {children}
   </Heading>
 );
 
 export const H2 = ({ children }: { children: React.ReactNode }) => (
-  <Heading as="h2" size="lg" pb="1rem">
+  <Heading as="h2" size="lg" pb="1rem" pt="1rem" mt="1rem" mb="1rem">
     {children}
   </Heading>
 );
 
 export const H3 = ({ children }: { children: React.ReactNode }) => (
-  <Heading as="h3" size="lg" pb="1rem">
+  <Heading as="h3" size="lg" pb="1rem" pt="1rem" mt="1rem" mb="1rem">
     {children}
   </Heading>
 );
 
 export const H4 = ({ children }: { children: React.ReactNode }) => (
-  <Heading as="h4" size="md" pb="1rem">
+  <Heading as="h4" size="md" pb="1rem" pt="1rem" mt="1rem" mb="1rem">
     {children}
   </Heading>
 );
 
 export const H5 = ({ children }: { children: React.ReactNode }) => (
-  <Heading as="h5" size="md" pb="1rem">
+  <Heading as="h5" size="md" pb="1rem" pt="1rem" mt="1rem" mb="1rem">
     {children}
   </Heading>
 );
 
 export const H6 = ({ children }: { children: React.ReactNode }) => (
-  <Heading as="h6" size="xl" pb="1rem">
+  <Heading as="h6" size="xl" pb="1rem" pt="1rem" mt="1rem" mb="1rem">
     {children}
   </Heading>
 );
 
 export const P = ({ children }: { children: React.ReactNode }) => (
-  <Text as="p" pb="1rem">
+  <Text as="p" pb="1rem" pt="1rem" mt="1rem" mb="1rem">
     {children}
   </Text>
 );
