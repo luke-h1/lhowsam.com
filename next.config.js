1990b0af460518ef3a20c2b2f3160e618869b90c
luke-h1 <luke.howsam@yahoo.com>
2021-04-21 16:19:41 +0100

    feat/add-sentry

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
-/* eslint-disable global-require */
+/* eslint-disable */
 const {
   WebpackBundleSizeAnalyzerPlugin,
 } = require('webpack-bundle-size-analyzer');
+const { withSentryConfig } = require('@sentry/nextjs');
 
 const { ANALYZE } = process.env;
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
 
 module.exports = {
   webpack: (config, { isServer }) => {
@@ -25,3 +36,5 @@ module.exports = {
     domains: ['lhowsam.com'],
   },
 };
+
+module.exports =  withSentryConfig(moduleExports, SentryWebpackPluginOptions)
