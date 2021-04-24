13707eed2ffea14bcff24a8f25819c5c5c0e1c4a
luke-h1 <luke.howsam@yahoo.com>
2021-04-24 20:07:11 +0100

    feat/improve-seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,9 +51,11 @@ const HomePage = ({
             React, Next, Graphql, Express & Node. You can check out some of my
             open source projects
             {' '}
-            <a className="text-black hover: underline dark:text-white">
-              <Link href="/projects">here</Link>
-            </a>
+            <Link href="/projects">
+              {' '}
+              <p className="text-black hover: underline dark:text-white" />
+              here
+            </Link>
             {' '}
             <hr className="text-black dark:text-white border border-solid" />
           </p>
