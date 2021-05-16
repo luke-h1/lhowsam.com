5e9b18c210edf660db59359a5a6da7f0ed5c6ee7
luke-h1 <luke.howsam@yahoo.com>
2021-05-16 20:47:09 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "starting deploy"
-npm run format && npm run lint && vc --prod -f && vc -f 
+npm run format && npm run lint && npm run test && npm run cy:ci && vc --prod -f && vc -f 
 echo "SUCCESFULLY DEPLOYED TO PRODUCTION"
