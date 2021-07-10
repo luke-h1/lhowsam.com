a2b369d37f4e1b962e6f1fd41e42de260bf6b63e
luke-h1 <luke.howsam@yahoo.com>
2021-07-10 22:24:21 +0100

    feat/redesign-entire-site - start on navbar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AiOutlineTwitter, AiOutlineGithub, AiOutlineLinkedin } from 'react-icons/ai';
+interface NavProps {
+
+}
+
+const Nav: React.FC<NavProps> = ({}) => {
+        return ();
+}
+export default Nav;
