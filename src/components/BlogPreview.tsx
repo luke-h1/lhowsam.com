c92fc804b53cc3da5b6fbe0cafe83c8f3abd2851
luke-h1 <luke.howsam@yahoo.com>
2021-07-11 18:59:44 +0100

    feat/fix-links-mobile

--- src/components/BlogPreview.tsx
+++ src/components/BlogPreview.tsx
@@ -26,21 +26,19 @@ const BlogPreview: React.FC<BlogPreviewProps> = ({ post }) => {
               </h2>
               <p className="leading-relaxed">{post.description}</p>
               <Link href={`/blog/${post.slug}`}>
-                <a className="text-indigo-500 inline-flex items-center mt-4">
-                  Read More
-                  <svg
-                    className="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M5 12h14" />
-                    <path d="M12 5l7 7-7 7" />
-                  </svg>
-                </a>
+                Read More
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M5 12h14" />
+                  <path d="M12 5l7 7-7 7" />
+                </svg>
               </Link>
             </div>
           </div>
