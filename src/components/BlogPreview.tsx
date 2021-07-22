f5c7be257cbb009ae531bf7561a38778415f09b0
luke-h1 <luke.howsam@yahoo.com>
2021-07-22 12:33:16 +0100

    fix seo issues

--- src/components/BlogPreview.tsx
+++ src/components/BlogPreview.tsx
@@ -8,7 +8,10 @@ interface BlogPreviewProps {
 
 const BlogPreview: React.FC<BlogPreviewProps> = ({ post }) => {
   return (
-    <section className="text-gray-600 body-font overflow-hidden">
+    <section
+      className="text-gray-600 body-font overflow-hidden"
+      key={post.slug}
+    >
       <div className="container px-5 py-24 mx-auto">
         <div className="-my-8 divide-y-2 divide-gray-100">
           <div className="py-8 flex flex-wrap md:flex-nowrap">
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,10 +12,10 @@ const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className="text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+        <p className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
           <SiCoderwall fontSize="35px" />
           <span className="ml-3 text-xl">Luke H</span>
-        </a>
+        </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
           <a
             href="https://github.com/luke-h1"
