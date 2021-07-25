bb793db3967af107c263fd9b30c0fd1bc85eed1f
luke-h1 <luke.howsam@yahoo.com>
2021-07-25 23:41:21 +0100

    feat/add-ui-unit-tests-for-nav

--- cypress/integration/nav.spec.ts
+++ cypress/integration/nav.spec.ts
@@ -1,19 +1,19 @@
-describe('nav contains correct text', () => { 
-    context('1080p res', () => { 
-        cy.viewport(1920, 1080)
-    before(() => { 
-        cy.visit('/')
-    })
+describe('blog page should work', () => {
+    context('1080p res', () => {
+      beforeEach(() => {
+        cy.viewport(1920, 1080);
+      });
+    });
+  
+    it('should render properly', () => {
+      cy.visit('/');
+      cy.get('[data-testid=nav-Home]').contains('Home').should('be.visible')
+      cy.get('[data-testid=nav-Blog]').contains('Blog').should('be.visible')
 
-    it('should contain correct text', () => { 
-        cy.get('[data-testid=nav-Home]').contains('Home').should('be.visible')
-        cy.get('[data-testid=nav-Blog]').contains('Blog').should('be.visible')
+      cy.get('[data-testid=nav-Projects]').contains('Projects').should('be.visible')
 
-        cy.get('[data-testid=nav-Projects]').contains('Projects').should('be.visible')
+      cy.get('[data-testid=nav-twitter]').should('be.visible')
+      cy.get('[data-testid=nav-github]').should('be.visible')
 
-        cy.get('[data-testid=nav-twitter]').should('be.visible')
-        cy.get('[data-testid=nav-github]').should('be.visible')
-
-    })
-  })
-})
+    });
+  });
