15e6ce685d90e79927cfe5b1d5a6f9ba13ed98c6
luke-h1 <luke.howsam@yahoo.com>
2021-07-25 22:49:10 +0100

    add less colors to Colors.ts

--- src/shared/Colors.ts
+++ src/shared/Colors.ts
@@ -1,8 +1,1 @@
-export const COLORS: string[] = [
-  '#93B7BE',
-  '#5BBA6F',
-  '#E1CE7A',
-  '#1E91D6',
-  '#E0E2DB',
-  '#BFB7B6',
-];
+export const COLORS: string[] = ['#93B7BE', '#5BBA6F', '#E1CE7A'];
