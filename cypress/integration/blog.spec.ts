2af57072cd3dee6b8ea6a321cff0243505ede490
luke-h1 <luke.howsam@yahoo.com>
2021-07-25 23:14:55 +0100

    test/update-ui-tests

--- cypress/integration/blog.spec.ts
+++ cypress/integration/blog.spec.ts
@@ -7,7 +7,7 @@ describe('blog page should work', () => {
 
   it('should render properly', () => {
     cy.visit('/blog');
-    cy.contains('Blog').should('exist');
-    cy.contains('Lessons learned, thoughts on various tech & articles on what I\'m building').should('exist')
+    cy.contains('Blog').should('be.visible')
+    cy.contains('Lessons learned, thoughts on various tech & articles on what I\'m building').should('be.visible')
   });
 });
