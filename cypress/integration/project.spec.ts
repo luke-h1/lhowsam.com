bdecbd4d389527dbc9056372490750ac1950c3da
luke-h1 <luke.howsam@yahoo.com>
2021-07-30 19:33:55 +0100

    feat/fix-ui-test-name

--- cypress/integration/project.spec.ts
+++ cypress/integration/project.spec.ts
@@ -24,7 +24,7 @@ describe('blog page should work', () => {
     cy.contains('this project was an eye opener on which technologies I most like working with (building full stack applications is really satisifying) and the importance of picking the correct tech-stack to start a project with. Initially the tech stack I chose to start this project was simply not suitable (i.e. Redux was overkill for this project).')
   })
 
-  it('should render lhowsam project', () => {
+  it('should render Automation project', () => {
     cy.visit('/projects'); 
     cy.contains('Automation').should('be.visible')
     cy.contains('I am a huge advocate for infrastructure automation & continuous integration as this allows developers to focus on innovating new features and solving new problems rather than being backlogged with maintaining existing infrastructure.In this project, I made Ansible playbooks & Bash scripts that automate the setup of my development environment & deployment environments (EC2, docker images etc.)').should('be.visible')
