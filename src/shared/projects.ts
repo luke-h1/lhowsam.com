de4cd04245e76c3554d5058266cc0cf67928a2c8
luke-h1 <luke.howsam@yahoo.com>
2021-08-02 19:52:07 +0100

    feat/add-airbnb-project

--- src/shared/projects.ts
+++ src/shared/projects.ts
@@ -17,9 +17,17 @@ export const Projects: {
   },
   {
     id: 2,
+    title: 'Airbnb clone - full stack clone of Airbnb',
+    description:
+      'In this project I built a clone of Airbnb with Next.js for the frontend & Graphql + Typescript for the api. While this project is not a 1 to 1 match of the actual Airbnb, I learnt a lot from this project (uploading & deleting s3 images), more complex SQL queries etc.',
+    githubUrl: 'https://github.com/luke-h1/airbnb-clone',
+    tech: ['Next.js', 'GraphQL', 'S3'],
+  },
+  {
+    id: 3,
     title: 'tmtodos.me - full stack todo app',
     description:
-      'this project was an eye opener on which technologies I most like working with (building full stack applications is really satisifying) and the importance of picking the correct tech-stack to start a project with. Initially the tech stack I chose to start this project was simply not suitable (i.e. Redux was overkill for this project).',
+      'This project was an eye opener on which technologies I most like working with (building full stack applications is really satisifying) and the importance of picking the correct tech-stack to start a project with. Initially the tech stack I chose to start this project was simply not suitable (i.e. Redux was overkill for this project).',
     siteUrl: 'https://tmtodos.me',
     githubUrl: 'https://github.com/luke-h1/tmtodos.me',
     tech: [
@@ -34,7 +42,7 @@ export const Projects: {
     ],
   },
   {
-    id: 3,
+    id: 4,
     title: 'Automation',
     description:
       'I am a huge advocate for infrastructure automation & continuous integration as this allows developers to focus on innovating new features and solving new problems rather than being backlogged with maintaining existing infrastructure.In this project, I made Ansible playbooks & Bash scripts that automate the setup of my development environment & deployment environments (EC2, docker images etc.)',
