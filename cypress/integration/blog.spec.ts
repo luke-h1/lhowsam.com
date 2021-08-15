7485fda2374fac8b4286cf0b930499b7a2b34499
Luke Howsam <55030296+luke-h1@users.noreply.github.com>
2021-08-15 16:50:42 +0100

    Update blog.spec.ts

--- cypress/integration/blog.spec.ts
+++ cypress/integration/blog.spec.ts
@@ -1,9 +1,7 @@
 describe('blog page should work', () => {
-  context('1080p res', () => {
     beforeEach(() => {
       cy.viewport(1920, 1080);
     });
-  });
 
   it('should render properly', () => {
     cy.visit('/blog');
