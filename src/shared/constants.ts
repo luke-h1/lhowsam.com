5aa933391a8f94c1f7550bfb60648252989719ba
Luke Howsam <55030296+luke-h1@users.noreply.github.com>
2021-08-15 17:21:27 +0100

    Update constants.ts

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -2,7 +2,7 @@ export namespace constants {
   export const fullName = 'Luke Howsam';
   export const firstName = 'Luke';
   export const site = 'lhowsam.com';
-  export const intro = "Hey I'm Luke 👋 | Software tester | React.js & GraphQL Enthusiast";
+  export const intro = "Hey I'm Luke 👋 | Software tester | React.js & Typescript Enthusiast";
   export const twitterHandle = '@lukeH_1999';
   export const FOCUS_VISIBLE_OUTLINE = 'focus:text-sky-500 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-sky-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none  rounded';
   export const GRADIENT_LINK = 'decoration-clone bg-clip-text font-medium text-transparent bg-gradient-to-br from-pink-500 via-red-500 to-yellow-500 hover:text-pink-600 hover:bg-none';
