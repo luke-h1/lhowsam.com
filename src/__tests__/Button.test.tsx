fc0c9847f0f018d5ef48072540754797190bbd98
luke-h1 <luke.howsam@yahoo.com>
2021-09-03 20:17:46 +0100

    feat/update-unit-tests

--- src/__tests__/Button.test.tsx
+++ src/__tests__/Button.test.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Button from '../components/Button';
 
 test('renders correctly', () => {
-  const { container } = render(<Button href="/test">Hello</Button>);
+  render(<Button href="/test">Hello</Button>);
 
-  const buttonLink = container.querySelectorAll('a');
-
-  expect(buttonLink[0]).toHaveAttribute('href', '/test');
-
-  expect(buttonLink[0]).toHaveTextContent('Hello');
+  expect(screen.getByRole('link')).toHaveAttribute('href', '/test')
+  expect(screen.getByText('Hello')).toBeInTheDocument()
 });
