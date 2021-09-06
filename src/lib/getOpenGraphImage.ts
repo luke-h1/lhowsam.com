7f3aa4acd22067ae240f9edaf7cf7f88c0a7884c
luke-h1 <luke.howsam@yahoo.com>
2021-09-06 21:34:52 +0100

    updates

--- src/lib/getOpenGraphImage.ts
+++
@@ -1,7 +0,0 @@
-import { OpenGraphImages } from 'next-seo/lib/types';
-
-export const getOpenGraphImage = (title: string):OpenGraphImages => ({
-  url: `https://lhowsam.com/api/og?title=${encodeURIComponent(title)}`,
-  width: 1200,
-  height: 630,
-});
