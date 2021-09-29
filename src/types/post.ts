a56a7daa119fdf33c14f34bd99523b5e3b5415eb
luke-h1 <luke.howsam@yahoo.com>
2021-09-29 08:19:41 +0100

    refactor-mdx-utils

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,16 +1,32 @@
-export interface PostMeta {
+// export interface PostMeta {
+//   title: string;
+//   createdAt: string;
+//   updatedAt: string;
+//   description: string;
+//   slug?: any;
+//   category: 'draft' | 'project' | 'post';
+//   image?: string;
+//   source?: string;
+//   isOnIndex?: string;
+// }
+
+// export type Post = {
+//   meta: PostMeta;
+//   code?: any;
+// };
+
+export interface Post {
   title: string;
   createdAt: string;
-  updatedAt: string;
-  description: string;
-  slug?: any;
-  category: 'draft' | 'project' | 'post';
-  image?: string;
-  source?: string;
-  isOnIndex?: string;
+  updatedAt: string | null;
+  slug: string;
+  content: string;
+  intro: string;
+  keywords: string;
+  readingTime: number;
+  draft: boolean;
+  isOnIndex: boolean;
+  frontmatter: {
+    [key: string]: any;
+  };
 }
-
-export type Post = {
-  meta: PostMeta;
-  code?: any;
-};
