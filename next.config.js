734289cc84ca54a652233225166ebb92fa794fa9
luke-h1 <luke.howsam@yahoo.com>
2021-10-04 07:48:07 +0100

    feat/fix-blog-404

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 module.exports = {
+  experimental: {
+    swcLoader: true,
+    swcMinify: false,
+  },
   reactStrictMode: true,
   generateBuildId: async () => {
     return '1';
+  },
+  async redirects() {
+    return [
+      {
+        source: '/blog/deploying-a-full-stack-app-via-dokku',
+        destination: '/blog/full-stack-deploy-with-dokku',
+        permanent: true,
+      },
+    ];
   },
 };
