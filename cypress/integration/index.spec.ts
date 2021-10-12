624220b6db68dc6048bc5223b0044efbd1d921de
luke-h1 <luke.howsam@yahoo.com>
2021-10-12 18:31:51 +0100

    fix unit & UI test

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -4,12 +4,7 @@ describe('index', () => {
   })
   it('should display correct index text (1)', () => {
     cy.get('h1').contains(
-        'Hey, I\'m Luke 👋'
-      ).should('be.visible')
-    });
-    it('should display correct index text (2)', () => {
-      cy.get('p').contains(
-        'Hey I\'m Luke, I currently work as a software tester where I do a mixture of manual & automated testing in an agile environment. I\'m also a developer who likes working with React & Node.js.'
+      'Hey, I\'m Luke 👋'
       ).should('be.visible')
     });
   });
