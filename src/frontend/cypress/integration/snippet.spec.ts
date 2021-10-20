21073a3d472c57d966e21df9a8bc0b50008b7ef3
luke-h1 <luke.howsam@yahoo.com>
2021-10-20 15:02:02 +0100

    feat/add-snippets (ui tests)

---
+++ src/frontend/cypress/integration/snippet.spec.ts
@@ -0,0 +1,10 @@
+describe('snippets', () => {
+  before(() => {
+    cy.visit('/code-snippets')
+  })
+  it('should display code snippets correctly', () => {
+    cy.get('h2').contains('Captialize first letter of a string in JSX').should('be.visible')
+
+    cy.get('h2').contains('Update user ID in postgreSQL ').should('be.visble')
+  })
+})
