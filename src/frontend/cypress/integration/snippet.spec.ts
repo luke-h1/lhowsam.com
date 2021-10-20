59d9d44c829288a3666b8f6a8da9c06907ec114b
luke-h1 <luke.howsam@yahoo.com>
2021-10-20 15:11:19 +0100

    fix typo

--- src/frontend/cypress/integration/snippet.spec.ts
+++ src/frontend/cypress/integration/snippet.spec.ts
@@ -5,6 +5,6 @@ describe('snippets', () => {
   it('should display code snippets correctly', () => {
     cy.get('h2').contains('Captialize first letter of a string in JSX').should('be.visible')
 
-    cy.get('h2').contains('Update user ID in postgreSQL ').should('be.visble')
+    cy.get('h2').contains('Update user ID in postgreSQL ').should('be.visible')
   })
 })
