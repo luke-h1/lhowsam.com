efcf2bc91d8674f2301f3bbc0dd5785a0fd6ea4f
luke-h1 <luke.howsam@yahoo.com>
2021-10-28 11:46:25 +0100

    fix(frontend): fix seo

--- src/frontend/src/utils/buildURL.ts
+++
@@ -1,4 +0,0 @@
-export const buildURL = (url: string, obj: object) => {
-  const query = Object.entries(obj).map(pair => pair.map(encodeURIComponent).join('=')).join('&');
-  return `${url}?${query}`
-}
