940074a3bb35a55b9a984bf72d3671a668ea6e04
luke-h1 <luke.howsam@yahoo.com>
2021-11-16 22:52:35 +0000

    fix(frontend): fix dark mode issues

--- src/frontend/src/components/Navbar.tsx
+++ src/frontend/src/components/Navbar.tsx
@@ -232,15 +232,12 @@ const NavbarMenu = () => {
   }, []);
 
   useEffect(() => {
-    if (darkTheme !== undefined) {
-      if (darkTheme) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        window.localStorage.setItem('theme', 'dark');
-      } else {
-        document.documentElement.removeAttribute('data-theme');
-        window.localStorage.setItem('theme', 'light');
-      }
+    if (darkTheme) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      window.localStorage.setItem('theme', 'dark');
     }
+    document.documentElement.removeAttribute('data-theme');
+    window.localStorage.setItem('theme', 'light');
   }, [darkTheme]);
 
   useEffect(() => {
