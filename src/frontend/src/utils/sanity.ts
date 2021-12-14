eba8c55bb8db598cef01d179224045ef6856d844
luke-h1 <luke.howsam@yahoo.com>
2021-12-14 14:57:43 +0000

    fix ci

--- src/frontend/src/utils/sanity.ts
+++ src/frontend/src/utils/sanity.ts
@@ -1,7 +1,7 @@
 import client from '@sanity/client';
 
 const cmsClient = client({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: 'wdj6ou2i',
   dataset: 'production',
   apiVersion: '2021-09-23',
   useCdn: process.env.NODE_ENV === 'production',
