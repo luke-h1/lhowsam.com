e2527eaa8ac3eecc64d51880194dc41da8177876
luke-h1 <luke.howsam@yahoo.com>
2021-12-24 12:17:25 +0000

    feat(frontend): init style commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app';
+import '@src/styles/global.css';
+import '@src/styles/prism.css';
 
 const App = ({ Component, pageProps, router }: AppProps) => {
   return <Component {...pageProps} />;
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <>
+      <p>hello</p>
+    </>
+  );
+};
+export default Home;
