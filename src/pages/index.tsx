8e0b680d57574e5696da5cfa4b0a9c50cf599d62
Luke Howsam <55030296+luke-h1@users.noreply.github.com>
2022-01-04 18:22:45 +0000

    Revert "Feat/add footer"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -73,7 +73,7 @@ const Home = ({ projects, posts }: Props) => {
       </div>
       <h2>Contact</h2>
       Get in touch via{' '}
-      <ul style={{ marginBottom: '2rem' }}>
+      <ul>
         <li>
           {' '}
           <a
