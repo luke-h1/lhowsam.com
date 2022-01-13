936ea03abcd7e9e6bbe060d51e207be0efdb9feb
luke-h1 <luke.howsam@yahoo.com>
2022-01-13 19:32:41 +0000

    add next-seo to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,47 @@
+import constants from '../constants';
+import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 import '../styles/global.css';
 import '../styles/prism.css';
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps, router }: AppProps) => {
+  const canonicalUrl = `${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`;
+
   return (
+    <>
+     <DefaultSeo
+        titleTemplate="%s | lhowsam.com"
+        title={constants.site}
+        canonical={canonicalUrl}
+        description={constants.intro}
+        openGraph={{
+          profile: {
+            firstName: 'Luke',
+            lastName: 'Howsam',
+            username: 'luke-h1',
+          },
+          type: 'website',
+          locale: 'en_GB',
+          url: canonicalUrl,
+          description: constants.intro,
+          site_name: 'lhowsam.com',
+          images: [
+            {
+              url: `${process.env.NEXT_PUBLIC_SITE_URL}/icons/logo.png`,
+              alt: constants.site,
+              width: 1200,
+              height: 630,
+            },
+          ],
+        }}
+        twitter={{
+          site: '@lukeH_1999',
+          cardType: 'summary_large_image',
+        }}
+      />
+
     <Component {...pageProps} />
+    </>
   );
 };
 export default App;
