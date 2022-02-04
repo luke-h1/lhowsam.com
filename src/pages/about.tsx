b987091ac220585152cc8383261d343a0c94420a
luke-h1 <luke.howsam@yahoo.com>
2022-02-04 15:13:16 +0000

    feat(frontend): add skills section

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,27 @@
 import Page from '@src/components/Page';
 import Photo from '@src/components/Photo';
+import SkillItem from '@src/components/SkillItem';
 import classnames from 'classnames';
 import { NextSeo } from 'next-seo';
 import { useRouter } from 'next/router';
+import { BiTestTube } from 'react-icons/bi';
+import {
+  SiDocker,
+  SiGit,
+  SiJavascript,
+  SiCypress,
+  SiAtlassian,
+  SiPython,
+  SiNextdotjs as SiNextDotJs,
+  SiNodedotjs as SiNodeDotJs,
+  SiSelenium,
+  SiReact,
+  SiGraphql,
+  SiCss3,
+  SiStyledcomponents as SiStyledComponents,
+  SiTypescript,
+  SiYarn,
+} from 'react-icons/si';
 import styles from './about.module.css';
 
 const AboutPage = () => {
@@ -38,7 +57,26 @@ const AboutPage = () => {
           technologies right now are: React.js, Typescript, GraphQL, Node.js,
           and PostgreSQL
         </p>
-        <h2>Contact</h2>
+        <h2>Skills</h2>
+        <ul className={styles.skillGrid}>
+          <SkillItem icon={SiJavascript} text="Javascript" />
+          <SkillItem icon={SiTypescript} text="TypeScript" />
+          <SkillItem icon={SiReact} text="React.js" />
+          <SkillItem icon={SiNextDotJs} text="Next.js" />
+          <SkillItem icon={SiCss3} text="CSS, SCSS" />
+          <SkillItem icon={SiNodeDotJs} text="Node.js" />
+          <SkillItem icon={SiGraphql} text="GraphQL" />
+          <SkillItem icon={BiTestTube} text="Automated & manual testing" />
+          <SkillItem icon={SiSelenium} text="Selenium" />
+          <SkillItem icon={SiPython} text="Python" />
+          <SkillItem icon={SiCypress} text="Cypress" />
+          <SkillItem icon={SiGit} text="Git, Github, Gitlab" />
+          <SkillItem icon={SiAtlassian} text="Jira, Trello, confluence etc." />
+          <SkillItem icon={SiDocker} text="Docker" />
+          <SkillItem icon={SiYarn} text="Yarn" />
+          <SkillItem icon={SiStyledComponents} text="styled-components" />
+        </ul>
+        <h3>Contact</h3>
         Get in touch via{' '}
         <ul>
           <li>
