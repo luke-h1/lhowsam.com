07d357d55fac5af5557644cc5c6e543617c2e7d5
luke-h1 <luke.howsam@yahoo.com>
2022-04-04 00:44:00 +0100

    fix: fix tests

--- cypress/integration/blog.spec.ts
+++ cypress/integration/blog.spec.ts
@@ -18,11 +18,11 @@ describe('blog page should work', () => {
     for (let i = 0; i < 8; i += 1) {
       
       cy.get('article > header').eq(i).find('a').should('have.attr', 'href').then((l) => {
-        links.push(`http://localhost:3000${l}`)
+        links.push(`${Cypress.config().baseUrl}${l}`)
         cy.visit(links[i])
         cy.get('h1').should('be.visible').and('not.be.empty');
         cy.getByTestId('blog-title').should('be.visible').and('not.be.empty')
-        cy.get('img').should('be.visible').and('not.be.empty')
+        cy.get('img').should('be.visible')
         cy.get('article').should('be.visible').and('not.be.empty')
         cy.visit('/blog');
         cy.get('header').find('h1').contains('Blog').should('be.visible')
--- cypress/integration/project.spec.ts
+++ cypress/integration/project.spec.ts
@@ -10,7 +10,7 @@ describe('projects', () => {
     for (let i = 0; i < 3; i += 1) {
       
       cy.get('article > header').eq(i).find('a').should('have.attr', 'href').then((l) => {
-        links.push(`http://localhost:3000${l}`)
+        links.push(`${Cypress.config().baseUrl}${l}`)
         cy.visit(links[i])
         cy.get('h1').should('be.visible').and('not.be.empty');
         cy.getByTestId('project-title').should('be.visible').and('not.be.empty')
