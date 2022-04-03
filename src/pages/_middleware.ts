433682826436dab589dd84dfafa312f36e2b2fc1
luke-h1 <luke.howsam@yahoo.com>
2022-04-03 16:03:26 +0100

    add _middleware.ts

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { NextFetchEvent, NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.twitter.com *.googletagmanager.com *.google-analytics.com *.fonts.gstatic.com *.media.graphassets.com;
+  child-src *.media.graphassets.com *.google.com *.googletagmanager.com *.google-analytics.com *.fonts.gstatic.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+  `;
+
+  const response = NextResponse.next();
+
+  response.headers.set(
+    'Content-Security-Policy',
+    ContentSecurityPolicy.replace(/\n/g, ''),
+  );
+  response.headers.set('Referrer-Policy', 'origin-when-cross-origin');
+  response.headers.set(
+    'Permissions-Policy',
+    'camera=(), microphone=(), geolocation=()',
+  );
+  response.headers.set(
+    'Strict-Transport-Security',
+    'max-age=31536000; includeSubDomains; preload',
+  );
+  response.headers.set('X-Frame-Options', 'DENY');
+  response.headers.set('X-Content-Type-Options', 'nosniff');
+  response.headers.set('X-DNS-Prefetch-Control', 'on');
+
+  return response;
+}
