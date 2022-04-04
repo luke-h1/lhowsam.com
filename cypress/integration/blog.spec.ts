c8f9b5d7510545bbae9e4342f8c63f08212ba92e
Luke Howsam <55030296+luke-h1@users.noreply.github.com>
2022-04-04 01:17:21 +0100

    Update blog.spec.ts

--- cypress/integration/blog.spec.ts
+++ cypress/integration/blog.spec.ts
@@ -2,21 +2,13 @@ describe('blog page should work', () => {
   before(() => {
     cy.visit('/blog');
     cy.get('header').find('h1').contains('Blog').should('be.visible')
-
   });
 
-  it('shows blog posts', () => {
-    for (let i = 0; i < 8; i += 1) {
-      cy.get('h2').eq(i).should('be.visible').and('not.be.empty');
-    }
-  });
-
-  it('shows slug page correctly', () => {
-
+  it('shows listing of blog posts & slug page correctly', () => {
     let links: string[] = []
 
     for (let i = 0; i < 8; i += 1) {
-      
+      cy.get('h2').eq(i).should('be.visible').and('not.be.empty');
       cy.get('article > header').eq(i).find('a').should('have.attr', 'href').then((l) => {
         links.push(`${Cypress.config().baseUrl}${l}`)
         cy.visit(links[i])
