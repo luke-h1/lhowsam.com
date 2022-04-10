6ae665f13d67f5dc4b745ca4172222d66a4d9e8e
luke-h1 <luke.howsam@yahoo.com>
2022-04-10 20:20:00 +0100

    fix project test

--- cypress/integration/project.spec.ts
+++ cypress/integration/project.spec.ts
@@ -11,7 +11,7 @@ describe('projects', () => {
     
 
     for (let i = 0; i < 3; i += 1) {
-      cy.get('h2').eq(i).should('be.visible').and('not.be.empty');
+      cy.get('h4').eq(i).should('be.visible').and('not.be.empty');
       cy.get('[data-testid="project-list"]').eq(i).find('a').should('have.attr', 'href').then((l) => {
         links.push(`${Cypress.config().baseUrl}${l}`)
         cy.visit(links[i])
