672907425a4ad74ece3de20ed217aa3f9c59cd5b
luke-h1 <luke.howsam@yahoo.com>
2022-04-15 17:11:05 +0100

    cleanup _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,27 +21,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link
-            rel="preload"
-            href="/fonts/GTWalsheimPro-Regular.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/fonts/GTWalsheimPro-Medium.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/fonts/GTWalsheimPro-Bold.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
