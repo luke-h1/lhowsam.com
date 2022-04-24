592a5984fbd2406465d7881547b205c668e74d60
luke-h1 <luke.howsam@yahoo.com>
2022-04-24 17:24:36 +0100

    chore(frontend): gen env types

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,6 +1,11 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NEXT_PUBLIC_GRAPHCMS_URL: string;
-    NEXT_PUBLIC_SITE_URL: string;
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_GRAPHCMS_URL: string;
+      NEXT_PUBLIC_SITE_URL: string;
+      GA_TRACKING_ID: string;
+    }
   }
 }
+
+export {};
