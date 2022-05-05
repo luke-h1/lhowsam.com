b6ef540ed0c84b1f00a330b5a14484fdc5431d79
luke-h1 <luke.howsam@yahoo.com>
2022-05-05 15:34:45 +0100

    feat(frontend): add server-side sitemap

--- src/frontend/next-sitemap.js
+++ src/frontend/next-sitemap.js
@@ -1,6 +1,11 @@
-const siteUrl = 'https://lhowsam.com';
+const siteUrl = process.env.NEXT_PUBLIC_URL;
 
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'],
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/' }],
+    additionalSitemaps: [`${siteUrl}/server-sitemap.xml`],
+  },
 };
