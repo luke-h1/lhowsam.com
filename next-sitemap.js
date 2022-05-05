fa1330d3106af61d0b352c7e2928fb0504c73203
luke-h1 <luke.howsam@yahoo.com>
2022-05-05 17:51:48 +0100

    feat(frontend): add indexing policies based on siteUrl

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,11 +1,23 @@
 const siteUrl = process.env.NEXT_PUBLIC_URL;
 
+function getPolicies() {
+  switch (siteUrl) {
+    case 'https://www.lhowsam.com':
+    case 'https://lhowsam.com/':
+      return [{ userAgent: '*', allow: '/' }];
+
+    default:
+      // prevent Google from crawling/indexing the site by default
+      return [{ userAgent: 'Googlebot', disallow: '/' }];
+  }
+}
+
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
   exclude: ['/server-sitemap.xml'],
   robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }],
+    policies: getPolicies(),
     additionalSitemaps: [`${siteUrl}/server-sitemap.xml`],
   },
 };
