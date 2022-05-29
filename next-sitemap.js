90e9af284439491d9d017d4f6f088eb015305103
luke-h1 <luke.howsam@yahoo.com>
2022-05-29 17:41:58 +0100

    fix(frontend): fix lint staged config

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,23 +1,11 @@
 const siteUrl = process.env.NEXT_PUBLIC_URL;
 
-function getPolicies() {
-  switch (siteUrl) {
-    case 'https://www.lhowsam.com':
-    case 'https://lhowsam.com/':
-      return [{ userAgent: '*', allow: '/' }];
-
-    default:
-      // prevent Google from crawling/indexing the site by default
-      return [{ userAgent: 'Googlebot', disallow: '/' }];
-  }
-}
-
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
   exclude: ['/server-sitemap.xml'],
   robotsTxtOptions: {
-    policies: getPolicies(),
+    policies: [{ userAgent: '*', allow: '/' }],
     additionalSitemaps: [`${siteUrl}/server-sitemap.xml`],
   },
 };
