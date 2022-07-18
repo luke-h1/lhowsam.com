ab35d1e64f83a5f48955bed1e9524699ed5c8a55
luke-h1 <luke.howsam@yahoo.com>
2022-07-18 23:00:40 +0100

    add about test

---
+++ cypress/e2e/about.cy.ts
@@ -0,0 +1,12 @@
+describe('about page', () => {
+  it('should display the about page', () => {
+    cy.visit('/about');
+    cy.contains('About');
+
+    cy.get('h1').should('contain', 'About').and('be.visible');
+    cy.get('p').should('be.visible');
+    cy.get('button').contains('Contact').should('be.visible');
+    cy.get('button').contains('Resume').should('be.visible');
+  })
+
+})
--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ const nextConfig = {
   },
   experimental: {
     legacyBrowsers: false,
-    images: { allowFutureImage: true },
   },
   generateBuildId: async () => {
     return '1';
