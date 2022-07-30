1fbcdd2c6038661f014d0097f4ac4d796678ae66
luke-h1 <luke.howsam@yahoo.com>
2022-07-30 22:08:47 +0100

    dry postService.ts

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -10,73 +10,44 @@ const slugsQuery = groq`
 }
 `;
 
+const postFields = `
+...,
+image {
+  alt,
+  asset {
+    _ref
+  },
+},
+tags[] -> {
+  title,
+  slug
+},
+slug {
+  current
+},
+`;
+
 const recentPostsQuery = groq`
 *[ _type == "post"][0...3] | order(publishedAt desc) {
-  ...,
-  image {
-    alt,
-    asset {
-      _ref
-    },
-  },
-  tags[] -> {
-    title,
-    slug
-  },
-  slug {
-    current
-  },
+  ${postFields}
 }
 `;
 
 const listAllPosts = groq`
   *[ _type == "post"] | order(publishedAt desc) {
-    ...,
-    image {
-      alt,
-      asset {
-      _ref
-     },
-    },
-    tags[]-> {
-      title,
-      slug
-   },
-    slug {
-      current
-    },
+    ${postFields}
   }
 `;
 
 const getPostQuery = groq`
 *[ _type == "post" && slug.current == $slug][0] {
-  ...,
-  image {
-    alt,
-    asset {
-      _ref
-    },
-  },
-  tags[]-> {
-    title,
-    slug
-  },
+  ${postFields}
 }
 `;
 
 const getPostByTagQuery = groq`
 *[_type == "post" && $keyword in tags[]->slug.current] {
-  ...,
-  image {
-    alt,
-    asset {
-      _ref
-    },
-  },
-  tags[]-> {
-  title,
-    slug
-  },
+  ${postFields}
 }
 `;
 
