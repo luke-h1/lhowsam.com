07735a70c003e5fd5db6cd5752158b7ae5b37756
luke-h1 <luke.howsam@yahoo.com>
2022-07-30 14:49:54 +0100

    finish share hocs

---
+++ src/components/share/EmailShareButton.ts
@@ -0,0 +1,33 @@
+import objectToGetParams from '@src/utils/objectToGetParams';
+import createShareButton from './hocs/createShareButton';
+
+interface Options {
+  body?: string;
+  separator?: string;
+  subject?: string;
+}
+
+function emailLink(url: string, { subject, body, separator }: Options) {
+  return `mailto:${objectToGetParams({
+    subject,
+    body: body ? body + separator + url : url,
+  })}`;
+}
+
+const EmailShareButton = createShareButton<Options>(
+  'email',
+  emailLink,
+  props => ({
+    subject: props.subject,
+    body: props.body,
+    separator: props.separator || ' ',
+  }),
+  {
+    openShareDialogOnClick: false,
+    onClick: (_, link: string) => {
+      window.location.href = link;
+    },
+  },
+);
+
+export default EmailShareButton;
---
+++ src/components/share/LinkedinShareButton.ts
@@ -0,0 +1,36 @@
+import assert from '@src/utils/assert';
+import objectToGetParams from '@src/utils/objectToGetParams';
+import createShareButton from './hocs/createShareButton';
+
+interface Options {
+  /** The url-encoded title value that you wish you use. */
+  title?: string;
+  /** The url-encoded description that you wish you use. */
+  summary?: string;
+  /** The url-encoded source of the content (e.g. your website or application name) */
+  source?: string;
+}
+
+function linkedinLink(url: string, { title, summary, source }: Options) {
+  assert(url, 'linkedin.url');
+
+  return `https://linkedin.com/shareArticle${objectToGetParams({
+    url,
+    mini: 'true',
+    title,
+    summary,
+    source,
+  })}`;
+}
+
+const LinkedinShareButton = createShareButton<Options>(
+  'linkedin',
+  linkedinLink,
+  ({ title, summary, source }) => ({ title, summary, source }),
+  {
+    windowWidth: 750,
+    windowHeight: 600,
+  },
+);
+
+export default LinkedinShareButton;
