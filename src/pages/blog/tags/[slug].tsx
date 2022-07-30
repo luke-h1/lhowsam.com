e85facfbe654994a3b0db7e9ac6f7c8ce9e79f0e
luke-h1 <luke.howsam@yahoo.com>
2022-07-30 19:51:22 +0100

    add tags slug page

---
+++ src/pages/blog/tags/[slug].tsx
@@ -0,0 +1,64 @@
+import Page from '@src/components/Page';
+import PostItem from '@src/components/PostItem';
+import siteConfig from '@src/config/site';
+import postService from '@src/services/postService';
+import { Post } from '@src/types/sanity';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import styles from '../index.module.scss';
+
+interface Props {
+  posts: Post[];
+}
+
+const TagPage = ({ posts }: Props) => {
+  const router = useRouter();
+
+  const slug = router.query.slug as string;
+
+  return (
+    <Page title={`Posts taged with ${slug.replace('-', ' ')}`}>
+      <div className={styles.postListing}>
+        {posts && (
+          <ul>
+            {posts.map(post => (
+              <li key={post._id}>
+                <PostItem post={post} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </Page>
+  );
+};
+
+export default TagPage;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const tags = await postService.getTagSlugs();
+  const paths = tags.map(tag => ({ params: { slug: tag.slug.current } }));
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  const slug = params?.slug as string;
+  if (!slug) {
+    return {
+      props: [],
+      notFound: true,
+    };
+  }
+
+  const posts = await postService.getPostsByTag(slug);
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: siteConfig.defaultRevalidate,
+  };
+};
