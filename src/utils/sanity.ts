7d402326cceacd2b395e0fa1520f6cf33888d0f4
luke-h1 <luke.howsam@yahoo.com>
2022-09-21 17:16:38 +0100

    feat(frontend): upgrade sanity version

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -3,7 +3,7 @@ import client from '@sanity/client';
 const studioClient = client({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: 'production',
-  apiVersion: '2022-03-20',
+  apiVersion: '2022-09-21',
   useCdn: process.env.NODE_ENV === 'production',
 });
 
