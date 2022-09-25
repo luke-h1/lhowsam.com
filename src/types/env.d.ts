982a728626b63af44a3d7ed53bb2ea562922a32f
luke-h1 <luke.howsam@yahoo.com>
2022-09-25 17:16:52 +0100

    chore(frontend): generate new environment variable types

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,6 +3,9 @@ declare global {
     interface ProcessEnv {
       NEXT_PUBLIC_SANITY_PROJECT_ID: string;
       NEXT_PUBLIC_URL: string;
+      SPOTIFY_CLIENT_ID: string;
+      SPOTIFY_CLIENT_SECRET: string;
+      SPOTIFY_REFRESH_TOKEN: string;
     }
   }
 }
