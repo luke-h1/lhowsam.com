a05dd7cac10b64abb46dfae505813567346cca48
luke-h1 <luke.howsam@yahoo.com>
2023-01-02 16:23:28 +0000

    chore(frontend): general cleanup

--- src/pages/server-sitemap.xml/index.tsx
+++ src/pages/server-sitemap.xml/index.tsx
@@ -11,15 +11,16 @@ export const getServerSideProps: GetServerSideProps = async ctx => {
     projectService.getSlugs(),
   ]);
 
-  const postFields = posts.map(post => ({
-    loc: `${process.env.NEXT_PUBLIC_URL}/blog/${post.slug.current}`,
-    lastmod,
-  }));
-
-  const projectFields = projects.map(project => ({
-    loc: `${process.env.NEXT_PUBLIC_URL}/projects/${project.slug.current}`,
-    lastmod,
-  }));
+  const [postFields, projectFields] = await Promise.all([
+    posts.map(post => ({
+      loc: `${process.env.NEXT_PUBLIC_URL}/blog/${post.slug.current}`,
+      lastmod,
+    })),
+    projects.map(project => ({
+      loc: `${process.env.NEXT_PUBLIC_URL}/projects/${project.slug.current}`,
+      lastmod,
+    })),
+  ]);
 
   const fields: ISitemapField[] = [...postFields, ...projectFields];
 
