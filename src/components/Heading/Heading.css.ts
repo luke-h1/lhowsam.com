89383a29af19c9f3ce167e4142a9ca1dd2f63d41
luke-h1 <luke.howsam@yahoo.com>
2023-01-22 16:39:58 +0000

    feat(frontend): add heading

---
+++ src/components/Heading/Heading.css.ts
@@ -0,0 +1,18 @@
+import { typography } from '@frontend/styles/typography.css';
+import { style, styleVariants } from '@vanilla-extract/css';
+
+export const root = style({
+  maxWidth: '60ch',
+  fontWeight: typography.fontWeight.bold,
+});
+
+export const tracking = styleVariants({
+  h1: {
+    letterSpacing: '-0.02em',
+  },
+  h2: {},
+  h3: {},
+  h4: {},
+  h5: {},
+  h6: {},
+});
