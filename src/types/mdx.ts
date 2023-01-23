6dedffd3bb4f11036ecf4e780cb53fd9f0c532d6
luke-h1 <luke.howsam@yahoo.com>
2023-01-23 20:04:58 +0000

    feat(frontend): refactor mdxToHtml.ts

--- src/types/mdx.ts
+++
@@ -1,9 +0,0 @@
-export type Variant =
-  | 'blue'
-  | 'green'
-  | 'orange'
-  | 'yellow'
-  | 'purple'
-  | 'black';
-
-export type Heading = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
--- src/utils/mdxToHtml.ts
+++ src/utils/mdxToHtml.ts
@@ -1,32 +1,60 @@
+/* eslint-disable no-param-reassign */
 import matter from 'gray-matter';
-import mdxPrism from 'mdx-prism';
 import { MDXRemoteSerializeResult } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
-import readingTime from 'reading-time';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
+import rehypeCodeTitles from 'rehype-code-titles';
+import rehypePrettyCode, { Options } from 'rehype-pretty-code';
+import remarkGfm from 'remark-gfm';
 import remarkCodeTitle from './remarkCodeTitle';
+
+const rehypePrettyCodeOptions: Partial<Options> = {
+  theme: {
+    light: 'github-light',
+    dark: 'github-dark',
+  },
+  tokensMap: {
+    // VScode command palette: Inspect Editor Tokens and Scopes
+    // https://github.com/Binaryify/OneDark-Pro/blob/47c66a2f2d3e5c85490e1aaad96f5fab3293b091/themes/OneDark-Pro.json
+    fn: 'entity.name.function',
+    objKey: 'meta.object-literal.key',
+  },
+  onVisitLine(node) {
+    // Prevent lines from collapsing in `display: grid` mode, and
+    // allow empty lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{ type: 'text', value: ' ' }];
+    }
+    node.properties.className.push('syntax-line');
+  },
+  onVisitHighlightedLine(node) {
+    node.properties.className.push('syntax-line--highlighted');
+  },
+  onVisitHighlightedWord(node) {
+    node.properties.className = ['syntax-word--highlighted'];
+  },
+};
 
 export interface MdxResult {
   compiledSource: MDXRemoteSerializeResult<Record<string, unknown>>;
-  timeToRead: number;
-  excerpt: string;
 }
 
 export default async function mdxToHtml(source: string): Promise<MdxResult> {
-  const { data, content, excerpt } = matter(source);
-  const timeToRead = readingTime(content).minutes;
+  const { data } = matter(source);
 
   const html = await serialize(source, {
     mdxOptions: {
-      remarkPlugins: [remarkCodeTitle],
-      rehypePlugins: [mdxPrism, rehypeAutolinkHeadings],
+      remarkPlugins: [remarkCodeTitle, remarkGfm],
+      rehypePlugins: [
+        rehypeCodeTitles,
+        [rehypePrettyCode, rehypePrettyCodeOptions],
+        rehypeAccessibleEmojis,
+      ],
     },
     scope: data,
   });
 
   return {
     compiledSource: html,
-    timeToRead,
-    excerpt: excerpt as string,
   };
 }
