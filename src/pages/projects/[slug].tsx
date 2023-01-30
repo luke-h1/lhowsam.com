18a9dfa96601102a0e6dfdd99e0ebd171c6d0430
luke-h1 <luke.howsam@yahoo.com>
2023-01-30 19:43:48 +0000

    feat(frontend): add project page

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,56 +1,40 @@
 import Box from '@frontend/components/Box/Box';
-import FormattedDate from '@frontend/components/FormattedDate';
 import Heading from '@frontend/components/Heading/Heading';
 import Components from '@frontend/components/MDXComponents';
 import Prose from '@frontend/components/Prose/Prose';
 import Spacer from '@frontend/components/Spacer/Spacer';
-import Text from '@frontend/components/Text/Text';
-import imageService from '@frontend/services/imageService';
-import postService from '@frontend/services/postService';
-import { Post } from '@frontend/types/sanity';
+import projectService from '@frontend/services/projectService';
+import { Project } from '@frontend/types/sanity';
 import mdxToHtml from '@frontend/utils/mdxToHtml';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
 import { NextSeo } from 'next-seo';
 
-// TODO: LH - revamp this page
-
 interface Props {
-  post: Post;
+  project: Project;
   compiledSource: MDXRemoteSerializeResult<
     Record<string, unknown>,
     Record<string, string>
   >;
 }
 
-const PostPage: NextPage<Props> = ({ post, compiledSource }) => {
+const PostPage: NextPage<Props> = ({ project, compiledSource }) => {
   const router = useRouter();
 
   return (
     <article>
       <NextSeo
-        title={post.title}
+        title={project.title}
         canonical={`https://lhowsam.com${router.asPath}`}
-        description={post.intro}
+        description={project.intro}
         openGraph={{
           defaultImageWidth: 1200,
           defaultImageHeight: 630,
-          images: [
-            {
-              url: imageService.urlFor(post.image.asset),
-              alt: post.title,
-              height: 1200,
-              width: 630,
-            },
-          ],
           url: `https://lhowsam.com${router.asPath}`,
-          title: `${post.title} | lhowsam.com`,
+          title: `${project.title} | lhowsam.com`,
           article: {
             authors: ['Luke Howsam'],
-            publishedTime: post.publishedAt,
-            tags: post.tags.map(tag => tag.title),
           },
         }}
       />
@@ -67,29 +51,10 @@ const PostPage: NextPage<Props> = ({ post, compiledSource }) => {
             marginBottom: '1rem',
           }}
         >
-          {post.title}
+          {project.title}
         </Heading>
-        <Box>
-          <Image
-            src={imageService.urlFor(post.image.asset)}
-            width={950}
-            height={330}
-            priority
-            placeholder="blur"
-            blurDataURL={imageService.urlFor(post.image.asset)}
-            style={{
-              borderRadius: '0.5rem',
-              objectFit: 'contain',
-            }}
-            alt={post.image.alt ?? post.title}
-          />
-        </Box>
         <Spacer height="sm" />
-        <Text as="time" dateTime={post.publishedAt} color="foregroundNeutral">
-          <FormattedDate>{post.publishedAt}</FormattedDate>
-        </Text>
       </Box>
-
       <Spacer height="xxxl" />
       <Box maxWidth="text" marginX="auto">
         <Prose>
@@ -112,19 +77,20 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
-  const post = await postService.getPost(params?.slug as string);
+  const project = await projectService.getProject(params?.slug as string);
 
-  if (!post) {
+  if (!project) {
     return {
       props: [],
       notFound: true,
     };
   }
-  const { compiledSource } = await mdxToHtml(post.content);
+
+  const { compiledSource } = await mdxToHtml(project.content);
 
   return {
     props: {
-      post,
+      project,
       compiledSource,
     },
   };
