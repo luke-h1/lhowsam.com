de991653486cf7201efc2be096625b366381ea59
luke-h1 <luke.howsam@yahoo.com>
2023-02-01 18:35:02 +0000

    test(frontend): add banner test

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -48,6 +48,7 @@ const Banner = ({ setOpen }: Props) => {
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
+          data-testid="cmdk-icon"
         >
           <path d="M18 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3H6a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3V6a3 3 0 0 0-3-3 3 3 0 0 0-3 3 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 3 3 0 0 0-3-3z" />
         </svg>
