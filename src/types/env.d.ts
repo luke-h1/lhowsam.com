bea799d2ca8b35a8c91c86fe7d68705ea1f564c7
luke-h1 <luke.howsam@yahoo.com>
2023-08-19 11:27:39 +0100

    chore: remove new relic

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,16 +1,14 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      NEXT_PUBLIC_SANITY_PROJECT_ID: string;
       NEXT_PUBLIC_URL: string;
       SPOTIFY_CLIENT_ID: string;
       SPOTIFY_CLIENT_SECRET: string;
       SPOTIFY_REFRESH_TOKEN: string;
       NEXT_PUBLIC_GA_TRACKING_ID: string;
-      SANITY_URL: string;
-      NEW_RELIC_LICENSE_KEY: string;
-      NEXT_PUBLIC_SANITY_PROJECT_ID: string;
     }
   }
 }
 
-export {};
+export {}
