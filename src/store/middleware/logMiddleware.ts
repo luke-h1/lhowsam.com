af8463508555178c6d078c3e6026471aaac0169d
luke-h1 <luke.howsam@yahoo.com>
2023-10-15 11:36:38 +0100

    feat(frontend): replace zustand with redux

---
+++ src/store/middleware/logMiddleware.ts
@@ -0,0 +1,15 @@
+import { PayloadAction } from '@reduxjs/toolkit';
+import cookies from 'js-cookie';
+import { Middleware } from 'redux';
+
+// Define the middleware
+export const logMiddleware: Middleware =
+  () => next => (action: PayloadAction<unknown>) => {
+    // use document.cookie="debug=1" to enable logging
+    if (cookies.get('debug') === 'true') {
+      // eslint-disable-next-line no-console
+      console.info('Action:', action);
+    }
+
+    return next(action);
+  };
