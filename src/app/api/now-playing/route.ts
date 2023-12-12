79b5b9eb8453d0e1ff1b9b7307fbd9d0e2f7824e
Luke Howsam <55030296+luke-h1@users.noreply.github.com>
2023-12-12 04:04:45 +0000

    fix(frontend): switch to using default runtime

--- src/app/api/now-playing/route.ts
+++ src/app/api/now-playing/route.ts
@@ -1,7 +1,6 @@
 import spotifyService from '@frontend/services/spotifyService';
 import { SongItem } from '@frontend/types/spotify';
 
-export const runtime = 'edge';
 export const revalidate = 8;
 
 export async function GET() {
