5adc877a5b37d0aab65a5f6fcb43e4c1b8865f90
luke-h1 <luke.howsam@yahoo.com>
2023-12-21 21:56:43 +0000

    feat(frontend): setup git-cz + commitlint

---
+++ commitzen.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat:\tAdding a new feature' },
+    { value: 'fix', name: 'fix:\tFixing a bug' },
+    {
+      value: 'style',
+      name: 'style: Add or update styles',
+    },
+    {
+      value: 'refactor',
+      name: 'refactor:\tCode change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'perf',
+      name: '\tCode change that improves performance',
+    },
+    {
+      value: 'test',
+      name: 'test:\tAdding tests cases / adds missing tests',
+    },
+    {
+      value: 'chore',
+      name: 'chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation',
+    },
+    { value: 'revert', name: 'revert:\tRevert to a commit' },
+  ],
+  scopes: [
+    { name: 'frontend' },
+    { name: 'infrastructure' },
+    { name: 'documentation' },
+    { name: 'ci' },
+  ],
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix', 'perf', 'refactor'],
+  subjectLimit: 100,
+  skipQuestions: ['body'],
+};
