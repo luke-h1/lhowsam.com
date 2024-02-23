f2e4ed6b1e81043327ae667a5d4eeae71fe8c8f4
luke-h1 <luke.howsam@yahoo.com>
2024-02-23 13:17:48 +0000

    feat(infrastructure): custom server for ECS

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import { createServer } from 'http';
+import next from 'next';
+import { parse } from 'url';
+
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true);
+    handle(req, res, parsedUrl);
+  }).listen(port);
+
+  // http basic auth
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`,
+  );
+});
