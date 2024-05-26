8df53bc18db81150171cfac52582c059a13bb15d
luke-h1 <luke.howsam@yahoo.com>
2024-05-26 11:03:17 +0100

    feat(frontend): add deployedBy in version check

--- src/app/api/version/route.ts
+++ src/app/api/version/route.ts
@@ -3,6 +3,8 @@ import pkg from '../../../../package.json';
 export async function GET() {
   const body = JSON.stringify({
     version: pkg.version,
+    deployedBy:
+      process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_AUTHOR_LOGIN ?? 'unknown',
   });
 
   return new Response(body, {
