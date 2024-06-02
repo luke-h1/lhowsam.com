421f02cb1650bba984d5732c1acc84912fdc484d
luke-h1 <luke.howsam@yahoo.com>
2024-06-02 19:16:04 +0100

    chore(frontend): add vercel envs to version endpoint

--- src/app/api/version/route.ts
+++ src/app/api/version/route.ts
@@ -3,8 +3,9 @@ import pkg from '../../../../package.json';
 export async function GET() {
   const body = JSON.stringify({
     version: pkg.version,
-    deployedBy:
-      process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_AUTHOR_LOGIN ?? 'unknown',
+    gitCommitRef: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ?? 'unknown',
+    tz: process.env.TZ ?? 'unknown',
+    lambdaVersion: process.env.AWS_LAMBDA_FUNCTION_VERSION ?? 'unknown',
   });
 
   return new Response(body, {
