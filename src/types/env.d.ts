644d0a9ebb50cc87acc314f2521930e187783eb5
luke-h1 <luke.howsam@yahoo.com>
2024-08-10 11:18:51 +0100

    feat(frontend): new relic

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -6,6 +6,8 @@ declare global {
       NEXT_PUBLIC_GA_TRACKING_ID: string;
       SANITY_DRAFT_SECRET: string;
       NEXT_PUBLIC_NOW_PLAYING_API_BASE_URL: string;
+      NEW_RELIC_APP_NAME: string;
+      NEW_RELIC_LICENSE_KEY: string;
     }
   }
 }
