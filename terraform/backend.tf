d0d8efcd3c455f5ceba2227ead6c9826abd402a3
luke-h1 <luke.howsam@yahoo.com>
2024-08-10 16:59:47 +0100

    fix(infra): backend.tf create

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,15 +1,15 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "lhowsam.com-tf-state"
-#     key     = "staging/terraform.tfstate"
-#     region  = "eu-west-2"
-#     encrypt = true
-#   }
+terraform {
+  backend "s3" {
+    bucket  = "lhowsam.com-tf-state"
+    key     = "staging/terraform.tfstate"
+    region  = "eu-west-2"
+    encrypt = true
+  }
 
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
