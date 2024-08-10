51b549080b6c2da86426c4268434d33ac5334031
luke-h1 <luke.howsam@yahoo.com>
2024-08-10 21:28:59 +0100

    feat(infra): staging infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
-provider "aws" {
-  region = "eu-west-2"
+data "aws_caller_identity" "current" {}
+
+module "single_zone" {
+  source  = "RJPearson94/open-next/aws//modules/tf-aws-open-next-zone"
+  version = ">= 2.0.0, < 3.0.0"
+
+  prefix      = "lho-${data.aws_caller_identity.current.account_id}"
+  folder_path = "./.open-next"
 }
