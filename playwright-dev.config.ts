ab4a3eefce7eec8f675db61c783aa7ad7c1535f9
luke-h1 <luke.howsam@yahoo.com>
2024-09-16 20:26:41 +0100

    chore(frontend): add basic auth back to stg

--- playwright-dev.config.ts
+++ playwright-dev.config.ts
@@ -4,20 +4,20 @@ import commonConfig from './playwright-common.config';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 
-// const btoa = (str: string) => Buffer.from(str).toString('base64');
-// const credentialsBase64 = btoa(
-//   `${process.env.BASIC_AUTH_USER}:${process.env.BASIC_AUTH_PASSWORD}`,
-// );
+const btoa = (str: string) => Buffer.from(str).toString('base64');
+const credentialsBase64 = btoa(
+  `${process.env.BASIC_AUTH_USER}:${process.env.BASIC_AUTH_PASSWORD}`,
+);
 
 const config: PlaywrightTestConfig = {
   ...commonConfig,
   workers: 2,
   fullyParallel: true,
-  // use: {
-  //   extraHTTPHeaders: {
-  //     Authorization: `Basic ${credentialsBase64}`,
-  //   },
-  // },
+  use: {
+    extraHTTPHeaders: {
+      Authorization: `Basic ${credentialsBase64}`,
+    },
+  },
 };
 
 export default config;
