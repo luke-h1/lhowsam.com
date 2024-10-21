c16230592cbb9c76c41817fe39573acd5182ce43
luke-h1 <luke.howsam@yahoo.com>
2024-10-21 20:39:01 +0100

    test(infrastructure): add failure notification for ui test runs (live)

--- scripts/ensure-rebased.sh
+++ scripts/ensure-rebased.sh
@@ -4,10 +4,10 @@ RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
 
-COMMITS_BEHIND_MAIN=$(eval "git rev-list --left-right --count origin/main...HEAD" | cut -f1)
+COMMITS_BEHIND_MAIN=$(eval "git rev-list --left-right --count origin/dev...HEAD" | cut -f1)
 
 if [ $COMMITS_BEHIND_MAIN -gt 0 ]; then
-  echo "${RED}Branch is behind main by $COMMITS_BEHIND_MAIN commits. Please rebase.${RESET}"
+  echo "${RED}Branch is behind dev by $COMMITS_BEHIND_MAIN commits. Please rebase ${RESET}"
 else
-  echo "${GREEN}Branch is in-sync with main.${RESET}"
+  echo "${GREEN}Branch is in-sync with dev.${RESET}"
 fi
