42034e47d000c040de90b666aa67149dfd9279b3
luke-h1 <luke.howsam@yahoo.com>
2024-10-26 17:30:13 +0100

    chore(frontend): add prom endpoint

---
+++ src/app/api/metrics/route.ts
@@ -0,0 +1,12 @@
+import { register, collectDefaultMetrics } from 'prom-client';
+
+export async function GET() {
+  return new Response(await register.metrics(), {
+    status: 200,
+    headers: {
+      'Content-Type': register.contentType,
+    },
+  });
+}
+
+collectDefaultMetrics({ prefix: 'omnidevfrontend_' });
