fbfcc6841e9f1614188dd2f3ed794b05309b45e0
luke-h1 <luke.howsam@yahoo.com>
2024-10-26 17:34:36 +0100

    fix(frontend): fix prometheus metrics endpoint

--- src/app/api/metrics/route.ts
+++ src/app/api/metrics/route.ts
@@ -1,8 +1,5 @@
 import { register, collectDefaultMetrics } from 'prom-client';
 
-const prefix =
-  process.env.NEXT_PUBLIC_URL === 'https://lhowsam.com' ? 'prod' : 'dev';
-
 export async function GET() {
   return new Response(await register.metrics(), {
     status: 200,
@@ -13,6 +10,5 @@ export async function GET() {
 }
 
 collectDefaultMetrics({
-  prefix: `lhowsam-${prefix}`,
-  labels: { app: 'lhowsam' },
+  prefix: 'lhowsam',
 });
