13d3cda15f77886c1aef21a54f9330a449cfc3ef
luke-h1 <luke.howsam@yahoo.com>
2024-10-26 17:31:07 +0100

    feat(frontend): add prometheus metrics endpoint

--- src/app/api/metrics/route.ts
+++ src/app/api/metrics/route.ts
@@ -1,5 +1,8 @@
 import { register, collectDefaultMetrics } from 'prom-client';
 
+const prefix =
+  process.env.NEXT_PUBLIC_URL === 'https://lhowsam.com' ? 'prod' : 'dev';
+
 export async function GET() {
   return new Response(await register.metrics(), {
     status: 200,
@@ -9,4 +12,7 @@ export async function GET() {
   });
 }
 
-collectDefaultMetrics({ prefix: 'omnidevfrontend_' });
+collectDefaultMetrics({
+  prefix: `lhowsam-${prefix}`,
+  labels: { app: 'lhowsam' },
+});
