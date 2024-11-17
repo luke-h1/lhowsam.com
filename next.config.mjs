537fa6e58cb769c196525fd77d2073c9fc794f30
luke-h1 <luke.howsam@yahoo.com>
2024-11-17 18:47:52 +0000

    fix(frontend): remove premature cf import

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const withVanillaExtract = createVanillaExtractPlugin();
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 const contentSecurityPolicy = `
  default-src 'self';
