8ea5f0e2987432c7ac93476fafae56512da2e716
luke-h1 <luke.howsam@yahoo.com>
2024-11-22 19:07:44 +0000

    fix(frontend): fix build

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import ErrorContent from '@frontend/components/ErrorContent';
-import newrelic from 'newrelic';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -10,9 +9,5 @@ export const metadata: Metadata = {
 };
 
 export default function ErrorPage() {
-  newrelic.recordLogEvent({
-    message: 'Error',
-    level: 'ERROR',
-  });
   return <ErrorContent statusCode="500" title="500 internal server error" />;
 }
