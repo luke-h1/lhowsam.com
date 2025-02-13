64d1368d6093888c2579f553e2a444e9650fb037
luke-h1 <luke.howsam@yahoo.com>
2025-02-13 21:43:47 +0000

    chore(frontend): update about

--- e2e/about.spec.ts
+++ e2e/about.spec.ts
@@ -11,11 +11,6 @@ test.describe('about', () => {
 
   test('should render correctly', async () => {
     await expect(page.locator("[data-testid='AboutPage-intro']")).toBeVisible();
-
     await expect(page.locator("a[href='/static/cv.pdf']")).toBeVisible();
-    await expect(
-      page.locator('[data-testid="experience-heading"]'),
-    ).toBeVisible();
-    await expect(page.locator('[data-testid="Skills"]')).toBeVisible();
   });
 });
