89bfbc80e54c82629557ac85ab359ee3c2a73668
luke-h1 <luke.howsam54@gmail.com>
2025-11-01 13:27:18 +0000

    chore(infrastructure): change git flow

--- src/utils/mdToHtml.ts
+++ src/utils/mdToHtml.ts
@@ -9,7 +9,6 @@ export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
     .use(remarkRehype, { allowDangerousHtml: true })
-    // @ts-expect-error - rehypePrettyCode types don't match unified plugin interface
     .use(rehypePrettyCode, {
       theme: 'catppuccin-macchiato',
       keepBackground: false,
